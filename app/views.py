09f51fbd0a3b8bc3e9e0311977f31eec2f51e037
Zambom <erikzambom@hotmail.com>
2016-09-01 16:11:01 -0300

    Template da home

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from courses.models import Course
+
 @login_required
 def index(request):
-	return render(request, "app/index.html")
+	context = {}
+
+	context['courses'] = Course.objects.filter(user = request.user)
+
+	return render(request, "home_app.html", context)
