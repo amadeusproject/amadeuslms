6adb9412b8b77c81ef8cc33a0b481885dadb6012
Matheus Lins <msl@cin.ufpe.br>
2016-09-05 04:40:48 -0300

    criando o test de editar perfil

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from rolepermissions.shortcuts import assign_role
+from django.core.urlresolvers import reverse
+from .models import *
+from .forms import *
 
 # Create your tests here.
+class TestCreateUser(TestCase):
+
+	def setUp(self):
+		self.client = Client()
+
+		self.user = User.objects.create_user(
+			username = 'test', 
+			email = 'testing@amadeus.com', 
+			is_staff = True, 
+			is_active = True, 
+			password = 'testing'
+		)
+		assign_role(self.user, 'system_admin')
+
+	def test_edit_users(self):
+		self.client.login(username='test', password='testing')
+
+		url = reverse('users:edit_profile', kwargs={'pk': self.user.id})        
+		data = EditUserForm(self.data['email']).data
+		data['email'] = "testing2@amadeus.com"
+
+		response = self.client.put(url, data, format='json')        
+		self.assertEqual(response.status_code, 200)        
+		self.assertEqual(response.data['email'], data['email'])
+
+
+
+
+
