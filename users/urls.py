ca4dcc0aecc74cd344a20b708b1a2623979dd9f4
Jailson Dias <jcd2@cin.ufpe.br>
2016-09-06 09:58:11 -0300

    Terminando a refatoração das urls #3

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^usuarios/$', views.UsersListView.as_view(), name='manage'),
-	url(r'^usuarios/criar/$', views.Create.as_view(), name='create'),
-	url(r'^usuario/editar/(?P<username>[\w_-]+)/$', views.Update.as_view(), name='update'),
-	url(r'^usuario/dados/(?P<username>[\w_-]+)/$', views.View.as_view(), name='view'),
-	url(r'^perfil/$', views.Profile.as_view(), name='profile'),
-	url(r'^perfil/editar/(?P<username>[\w_-]+)/$', views.EditProfile.as_view(), name='edit_profile'),
+	url(r'^users/$', views.UsersListView.as_view(), name='manage'),
+	url(r'^users/create/$', views.Create.as_view(), name='create'),
+	url(r'^user/edit/(?P<username>[\w_-]+)/$', views.Update.as_view(), name='update'),
+	url(r'^user/data/(?P<username>[\w_-]+)/$', views.View.as_view(), name='view'),
+	url(r'^profile/$', views.Profile.as_view(), name='profile'),
+	url(r'^profile/edit/(?P<username>[\w_-]+)/$', views.EditProfile.as_view(), name='edit_profile'),
 ]
