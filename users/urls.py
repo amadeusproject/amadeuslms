f512e4f6fce77753d58a8036dd7354e4a21e327c
Zambom <erikzambom@hotmail.com>
2016-09-07 00:12:08 -0300

    Refactor users urls [Issue #3]

--- users/urls.py
+++ users/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^usuarios/$', views.UsersListView.as_view(), name='manage'),
-	url(r'^usuarios/criar/$', views.Create.as_view(), name='create'),
-	url(r'^usuario/editar/(?P<username>[\w_-]+)/$', views.Update.as_view(), name='update'),
-	url(r'^usuario/dados/(?P<username>[\w_-]+)/$', views.View.as_view(), name='view'),
-	url(r'^perfil/$', views.Profile.as_view(), name='profile'),
-	url(r'^perfil/editar/(?P<username>[\w_-]+)/$', views.EditProfile.as_view(), name='edit_profile'),
+	url(r'^$', views.UsersListView.as_view(), name='manage'),
+	url(r'^create/$', views.Create.as_view(), name='create'),
+	url(r'^edit/(?P<username>[\w_-]+)/$', views.Update.as_view(), name='update'),
+	url(r'^view/(?P<username>[\w_-]+)/$', views.View.as_view(), name='view'),
+	url(r'^profile/$', views.Profile.as_view(), name='profile'),
+	url(r'^profile/editar/(?P<username>[\w_-]+)/$', views.EditProfile.as_view(), name='edit_profile'),
 	#
 	url(r'^profile/update/$', views.UpdateUser.as_view(), name='update_user'), 
 ]
