1926d59e65f81a8acf2ce45b28616e28c8bcffa8
Ailson da Cruz <acgt@cin.ufpe.br>
2016-09-09 15:19:21 -0300

    Edit user test views and url #7

--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
 	url(r'^profile/$', views.Profile.as_view(), name='profile'),
 	url(r'^profile/editar/(?P<username>[\w_-]+)/$', views.EditProfile.as_view(), name='edit_profile'),
 	#
-	url(r'^profile/update/$', views.UpdateUser.as_view(), name='update_user'), 
+	url(r'^profile/update/$', views.UpdateUser.as_view(), name='update_profile'), 
 ]
