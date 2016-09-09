4dc3a1374e717e3fe9be7be890befe52bf3c055f
fbormann <felipe.bormann@gmail.com>
2016-09-08 23:47:53 -0300

    modified header to accept notifications and created notification system, stil has bugs from Python 2.7 implementation on inheritance, had to duplicate code

--- core/decorators.py
+++ core/decorators.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from functools import wraps
-from .models import Action, Resource, Action_Resource, Log
+from .models import Action, Resource, Action_Resource, Log, Notification
 
 def log_decorator(log_action = '', log_resource = ''):
 
@@ -45,3 +45,48 @@ def log_decorator(log_action = '', log_resource = ''):
 		return wraps(view_function)(_decorator)
 
 	return _log_decorator
+
+
+def notification_decorator(read = False, message = '', actor = None, users = [], not_action='', not_resource=''):
+	
+	def _notification_decorator(view_function):
+
+		def _decorator(request, *args, **kwargs):
+			#Do something before the call
+
+			response = view_function(request, *args, **kwargs)
+			action = Action.objects.filter(name = not_action)
+			resource = Resource.objects.filter(name = not_resource)
+
+			if not action:
+				action = Action(name = not_action)
+				action.save()
+			else:
+				action = action[0]
+
+			if not resource:
+				resource = Resource(name = not_resource)
+				resource.save()
+			else:
+				resource = resource[0]
+
+			action_resource = Action_Resource.objects.filter(action = action, resource = resource)
+
+			if not action_resource:
+				action_resource = Action_Resource(action = action, resource = resource)
+				action_resource.save()
+			else:
+				action_resource = action_resource[0]
+			
+			if request.user.is_authenticated: #the user was authenticated by the view
+				notification = Notification(actor = request.user, message= message, 
+					action_resource = action_resource, user = request.user)
+
+					
+			
+			#Do something after the call
+			return response
+
+		return wraps(view_function)(_decorator)
+	
+	return _notification_decorator
