fda8332eee628a841f7b58157153219124ac9a5d
Zambom <erikzambom@hotmail.com>
2016-09-15 23:35:46 -0300

    Fixing users no image display and updating admin's user form

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,9 @@
 # coding=utf-8
-
+import os
+from django.conf import settings
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from rolepermissions.shortcuts import assign_role
 from .models import User
 
 
@@ -25,10 +27,32 @@ class ProfileForm(forms.ModelForm):
 		}	
 
 class UserForm(forms.ModelForm):
+	def save(self, commit=True):
+		super(UserForm, self).save(commit=False)
+
+		#if not self.instance.image:
+		#	self.instance.image = os.path.join(os.path.dirname(settings.BASE_DIR), 'uploads', 'no_image.jpg')
+
+		self.instance.set_password(self.cleaned_data['password'])
+		self.instance.save()
+
+		if self.instance.is_staff:
+			assign_role(self.instance, 'system_admin')
+		elif self.instance.type_profile == 2:
+			assign_role(self.instance, 'student')
+		elif self.instance.type_profile == 1:
+			assign_role(self.instance, 'professor')
+
+		self.instance.save()
+
+		return self.instance
 
 	class Meta:
 		model = User
-		fields = ['username', 'name', 'email', 'birth_date', 'city', 'state', 'gender', 'type_profile', 'cpf', 'phone', 'image', 'is_staff', 'is_active']
+		fields = ['username', 'name', 'email', 'password', 'birth_date', 'city', 'state', 'gender', 'type_profile', 'cpf', 'phone', 'image', 'is_staff', 'is_active']
+		widgets = {
+			'password':forms.PasswordInput
+		}
 
 class EditUserForm(forms.ModelForm):
 
