2a6cf86ac4be449dfdffc1fa14aad84e42649043
Zambom <erikzambom@hotmail.com>
2016-09-20 22:09:07 -0300

    Model of forum's post [Issue: #82]

--- forum/models.py
+++ forum/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from autoslug.fields import AutoSlugField
 
 from courses.models import Activity
+from users.models import User
 
 """
 It's one kind of activity available for a Topic.
@@ -12,7 +13,7 @@ It works like a 'topic' of forum, which users can post to it and answer posts of
 class Forum(Activity):
 	title = models.CharField(_('Title'), max_length = 100)
 	description = models.TextField(_('Description'), blank = True)
-	create_date = models.DateField(_('Create Date'), auto_now_add = True)
+	create_date = models.DateTimeField(_('Create Date'), auto_now_add = True)
 
 	class Meta:
 		verbose_name = _('Forum')
@@ -20,3 +21,18 @@ class Forum(Activity):
 
 	def __str__(self):
 		return self.title
+
+"""
+It represents a post made in a forum (topic)
+"""
+class Post(modes.Model):
+	user = models.ForeignKey(User, verbose_name = _('Autor'))
+	message = models.TextField(_('Post message'), blank = False)
+	post_date = models.DateTimeField(_('Post Date'), auto_now_add = True)
+
+	class Meta:
+		verbose_name = _('Post')
+		verbose_name_plural = _('Posts')
+
+	def __str__(self):
+		return ''.join([self.user.name, " / ", self.post_date])
