4a7593e5e2aa5c6a0a4fc95b40cc98363d71d6a9
Zambom <erikzambom@hotmail.com>
2016-09-23 06:54:04 -0300

    Putting *.html files in folders [Issue: #73]

--- forum/views.py
+++ forum/views.py
@@ -13,7 +13,7 @@ class ForumIndex(LoginRequiredMixin, generic.ListView):
 	login_url = reverse_lazy("core:home")	
 	redirect_field_name = 'next'
 
-	template_name = "forum_list.html"
+	template_name = "forum/forum_list.html"
 	context_object_name = 'foruns'
 
 	def get_queryset(self):
@@ -25,7 +25,7 @@ class ForumIndex(LoginRequiredMixin, generic.ListView):
 
 class CreateForumView(LoginRequiredMixin, generic.edit.CreateView):
 
-	template_name = 'forum_form.html'
+	template_name = 'forum/forum_form.html'
 	form_class = ForumForm
 	success_url = reverse_lazy('forum:index')
 
@@ -33,7 +33,7 @@ class PostIndex(LoginRequiredMixin, generic.ListView):
 	login_url = reverse_lazy("core:home")	
 	redirect_field_name = 'next'
 
-	template_name = "post_list.html"
+	template_name = "post/post_list.html"
 	context_object_name = 'posts'
 
 	def get_queryset(self):
@@ -47,7 +47,7 @@ class PostAnswerIndex(LoginRequiredMixin, generic.ListView):
 	login_url = reverse_lazy("core:home")	
 	redirect_field_name = 'next'
 
-	template_name = "post_answer_list.html"
+	template_name = "post_answers/post_answer_list.html"
 	context_object_name = 'answers'
 
 	def get_queryset(self):
