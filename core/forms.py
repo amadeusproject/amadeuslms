34d6ebe7d1335370b1639be38090fab2018f9f01
ailsoncgt <acgt@cin.ufpe.br>
2016-09-28 16:33:43 -0300

    Validate cpf in forms.py #14

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from users.models import User
+from pycpfcnpj import cpfcnpj
+import re
+
+
 
 class RegisterUserForm(forms.ModelForm):
 
@@ -8,6 +12,37 @@ class RegisterUserForm(forms.ModelForm):
     password2 = forms.CharField(label = _('Password confirmation'), widget = forms.PasswordInput)
     # birth_date = forms.DateField(widget=forms.SelectDateWidget())
     MIN_LENGTH = 8
+
+    def validate_cpf(self, cpf):
+        cpf = ''.join(re.findall('\d', str(cpf)))
+        # print(cpf)
+
+        # if (not cpf) or (len(cpf) < 11):
+        #     return False
+
+        # #Get only the first 9 digits and generate other 2
+        # _int = map(int, cpf)
+        # integer = list(map(int, cpf))
+        # new = integer[:9]
+
+        # while len(new) < 11:
+        #     r = sum([(len(new) + 1 - i)* v for i, v in enumerate(new)]) % 11
+
+        #     if r > 1:
+        #         f = 11 - r
+        #     else:
+        #         f = 0
+        #     new.append(f)
+
+        # #if generated number is the same(original) the cpf is valid
+        # new2 = list(new)
+        # if new2 == _int:
+        #     return cpf
+        # else:
+        #     return False
+        if cpfcnpj.validate(cpf):
+            return True
+        return False
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -19,7 +54,8 @@ class RegisterUserForm(forms.ModelForm):
         cpf = self.cleaned_data['cpf']
         if User.objects.filter(cpf = cpf).exists():
             raise forms.ValidationError(_('There is already a registeres User with this CPF'))
-        print(cpf)
+        if not self.validate_cpf(cpf):
+            raise forms.ValidationError(_('Please enter a valid CPF'))
         return cpf
 
     def clean_password(self):
