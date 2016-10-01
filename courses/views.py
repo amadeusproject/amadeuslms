d0f762481e923d40b9904474dab2fea6cb0c062f
Matheus Lins <msl@cin.ufpe.br>
2016-10-01 18:35:35 -0300

    updating view course in teacher

--- courses/views.py
+++ courses/views.py
@@ -24,13 +24,14 @@ class IndexView(LoginRequiredMixin, NotificationMixin, generic.ListView):
 	redirect_field_name = 'next'
 	queryset = Course.objects.all()
 	template_name = 'course/index.html'
-	context_object_name = 'subjects_student'
+	context_object_name = 'courses_student'
 	paginate_by = 3
 
 	def get_context_data(self, **kwargs):
 		context = super(IndexView, self).get_context_data(**kwargs)
 		context['categories'] = Category.objects.all()
-		context['subjects_teacher'] = Course.objects.filter(professors__name = self.request.user.name)
+		context['courses_teacher'] = Course.objects.filter(professors__name = self.request.user.name)
+
 		return context
 
 class CreateCourseView(LoginRequiredMixin, HasRoleMixin, NotificationMixin,generic.edit.CreateView):
