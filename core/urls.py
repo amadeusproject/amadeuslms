7d7c7583cad99cd347c86b2ee06fbd3eab059f96
ifac0 <ifac@cin.ufpe.br>
2016-10-01 11:58:08 -0300

    add app remeber password #58

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import password_reset, password_reset_done,password_reset_confirm, password_reset_complete
 
 from . import views
 
@@ -11,4 +12,10 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': 'core:home'}, name='logout'),
     url(r'^notification/([0-9]+)/$', views.processNotification, name='notification_read'),
     url(r'^getNotifications/$', views.getNotifications, name='getNotifications'),
+
+	url(r'^reset/$', password_reset, {'template_name':'registration/passwor_reset_form.html','email_template_name':'registration/password_reset_email.html','post_reset_redirect':'done/'}, name="password_reset"),
+	url(r'^reset/done/$', password_reset_done, {'template_name':'registration/passwor_reset_done.html'}),
+	url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name':'registration/password_reset_confirm.html'}),
+	url(r'^done/$', password_reset_complete,{'template_name':'registration/passwor_reset_complete.html'}),
+
 ]
