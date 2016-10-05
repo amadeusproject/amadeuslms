6a60d3e2f2f79b0c4df7f95092748ce106bf4ede
Matheus Lins <msl@cin.ufpe.br>
2016-10-05 18:02:28 -0300

    updating the subject's model

--- courses/models.py
+++ courses/models.py
@@ -55,6 +55,7 @@ class Subject(models.Model):
 	update_date = models.DateTimeField(_('Date of last update'), auto_now=True)
 	course = models.ForeignKey(Course, verbose_name = _('Course'), related_name="subjects")
 	professors = models.ManyToManyField(User,verbose_name=_('Professors'), related_name='subjects')
+	students = models.ManyToManyField(User,verbose_name=_('Students'), related_name='subject_student')
 
 
 	class Meta:
