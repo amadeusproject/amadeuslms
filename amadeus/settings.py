ff89eb8c4c1a868661859d6852ddd1974faa6b6e
fbormann <felipe.bormann@gmail.com>
2016-10-05 11:42:41 -0300

    added whitenoise package and modified settings for serving static files heroku

--- amadeus/settings.py
+++ amadeus/settings.py
@@ -142,8 +142,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#Static files heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static")
 ]
 
 # Files
