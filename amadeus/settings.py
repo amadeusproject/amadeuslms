2003d55e35add86c2723dc2be0bcb22bb532b5e4
fbormann <felipe.bormann@gmail.com>
2016-10-05 11:48:55 -0300

    following heroku static files tutorial

--- amadeus/settings.py
+++ amadeus/settings.py
@@ -145,6 +145,8 @@ STATIC_URL = '/static/'
 #Static files heroku
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
