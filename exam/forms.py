228f185e29817672baa3d83c23cd606c2c0a5013
ifac0 <ifac@cin.ufpe.br>
2016-10-06 13:09:23 -0300

    desenvolvendo forms de exam #171

---
+++ exam/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Exam
+
+class ExamForm(forms.ModelForm):
+	def clean_end_date(self):
+		beginDate = self.data['beginDate']
+		endDate = self.data['endDate]
+
+		if beginDate and endDate and endDate < beginDate:
+			raise forms.ValidationError(_('The end date may not be before the start date.'))
+		return endDate
+
+	def clean_begin_date(self):
+		endDate = self.data['endDate']
+		beginDate = self.data['beignDate']
+
+		if enDate and benginDate and beginDate <= endDate:
+			raise forms.ValidationError(_('The exam start date must be after the end of registration.'))
+		return beginDate
+
+	def clean_end_date(self):
+		beginDate = self.data['beginDate']
+		endDate = self.data['endDate']
+
+		if beginDate and endDate and endDate < beginDate:
+			raise forms.ValidationError(_('The finish date may not be before the start date.'))
+		return end_date
+
+
+
+    class Meta:
+        model = Exam
+        fields = ['name','beginDate','endDate']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Exam?'}),
+            'beginDate': forms.DateTimeInput(
+                attrs={'placeholder': 'Start date to resolve the exam'}),
+            'endDate': forms.DateTimeInput(
+                attrs={'placeholder': 'Finish date permited to resolve the exam'}),
+        }
