6ad20d2a07b1fb6f544a58bfff3ba5fcf8803777
Gustavo Bernardo <Gustavo Bernardo>
2016-10-06 14:21:13 -0300

    Solving some attribute problems [Issue #119]

--- links/models.py
+++ links/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from courses.models import Material
 # Create your models here.
-class Link(Material,models.Model):
-	name = models.CharField(max_lenght = 100)
-	link = models.UrlField()
-	description = models.CharField(max_lenght = 200)
+class Link(models.Model):
+	name = models.CharField(max_length=100)
+	link = models.URLField()
+	description = models.CharField(max_length=200)
 	class Meta:
 		verbose_name = 'Link'
 		verbose_name_plural = "Links"
 	def __str__(self):
 		return str(self.name)
-	
-
-
