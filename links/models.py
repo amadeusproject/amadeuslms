210092459e238a40154abdee7dd4fb3be58dfe64
Gustavo Bernardo <Gustavo Bernardo>
2016-10-06 23:27:01 -0300

    problems solved. [Issue #119]

--- links/models.py
+++ links/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from courses.models import Material
+from autoslug.fields import AutoSlugField
 # Create your models here.
 class Link(models.Model):
 	name = models.CharField(max_length=100)
