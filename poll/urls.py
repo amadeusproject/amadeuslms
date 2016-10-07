1be45ecb091194052d1839f8fbeed9a0b51df948
Jailson Dias <jcd2@cin.ufpe.br>
2016-10-06 22:34:38 -0300

    Criado o template e o procedimento de remover enquete #114

--- poll/urls.py
+++ poll/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
 	url(r'^create/(?P<slug>[\w\-_]+)/$', views.CreatePoll.as_view(), name='create_poll'), # topic slug
 	url(r'^update/(?P<slug>[\w\-_]+)/$', views.UpdatePoll.as_view(), name='update_poll'), # poll slug
+	url(r'^teste/(?P<slug>[\w\-_]+)/$', views.CreatePollModal.as_view(), name='teste'), # poll
+	url(r'^mymodal/', views.MyModal.as_view(), name='mymodal'),
+	url(r'^delete/(?P<slug>[\w\-_]+)/$', views.DeletePoll.as_view(), name='delete_poll'), # poll
 
 ]
