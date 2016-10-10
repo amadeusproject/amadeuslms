c77ab4d1efc65c1d680013d17bdf2649004cef0c
ailsoncgt <acgt@cin.ufpe.br>
2016-10-10 11:43:50 -0300

    Show user fields in admin.py

--- users/admin.py
+++ users/admin.py
@@ -5,6 +5,6 @@ from .forms import AdminUserForm
 class UserAdmin(admin.ModelAdmin):
 	list_display = ['username', 'name', 'email', 'is_staff', 'is_active']
 	search_fields = ['username', 'name', 'email']
-	form = AdminUserForm
+	# form = AdminUserForm
 
 admin.site.register(User, UserAdmin)
