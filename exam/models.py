289e1517a04dd3fadd95f4d23fd7327405f3f91c
ifac0 <ifac@cin.ufpe.br>
2016-10-18 17:58:37 -0300

    atualização do admin, form, model, url, views e permissions #171

--- exam/models.py
+++ exam/models.py
@@ -27,4 +27,18 @@ class Answer(models.Model):
         verbose_name_plural = _('Answers')
 
     def __str__(self):
-        return str(self.answer) + str("/") + str(self.poll)
+        return str(self.answer) + str("/") + str(self.exam)
+
+class AnswersStudent(models.Model):
+    status = models.BooleanField(_("Answered"), default=False)
+    exam = models.ForeignKey(Exam, verbose_name = _('Exam'), related_name='answers_stundet')
+    answer = models.ManyToManyField(Answer,verbose_name = _('Answers Students'), related_name='answers_stundet')
+    student = models.ForeignKey(User, verbose_name = _('Student'), related_name='answers_stundent')
+    answered_in = models.DateTimeField(_("Answered Date"),auto_now=True)
+
+    class Meta:
+        verbose_name = _('Answer Stundent')
+        verbose_name_plural = _('Answers Student')
+
+    def __str__(self):
+        return str(self.student) + str("/") + str(self.exam)
