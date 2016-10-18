e6e65704fe159f85ef6089a456f58ac0f2272985
Gustavo Bernardo <Gustavo Bernardo>
2016-10-18 11:30:36 -0300

    Updating admin to solve issues with models [Issue: #126]

--- links/admin.py
+++ links/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Link
 
 class LinkAdmin(admin.ModelAdmin):
-	list_display = ['name', 'link','description']
-	search_fields = ['name', 'link','description']
+	list_display = ['name', 'link_url','link_description']
+	search_fields = ['name', 'link_url','link_description']
 
 
 admin.site.register(Link, LinkAdmin)
