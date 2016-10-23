b8afe62ebe2c7b8f5566588a939c861437e02c0e
fbormann <felipe.bormann@gmail.com>
2016-10-23 17:11:29 -0300

    added translation files

---
+++ files/templates/files/update_file.html.py
@@ -0,0 +1,120 @@
+BBBB BBBBBB BBBBBBBBBBBBB BBBB
+
+XXXX XXXXX XXXXXX XXXX XXX
+XXXXX XXXXXXXXXXXXXXXX XXXXXXXXXXXXXXX XXXXXXBBBBBB BBBBBBBBBBBBBBXX
+
+XXXX XXXXXXXXXXXXXXXXXXXX
+    XXXX XXXXXXXXXXXX XXXXX XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXX XXXX
+        XXXX XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXX
+            XXXX XXXXXXXXXXXXXXXXXXXXXX
+                XXXX XXXXXXXXXXXXXXXXXXXXX
+                    XXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    XXX XXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXX gettext(u'Edit File') XXXXX
+                XXXXXX
+                XXXX XXXXXXXXXXXXXXXXXXX
+                XXXX XXXX XXX
+                    XXXXX XXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    BBBBBBBBBB
+                        BB BBBBBBBB
+                            BBB BBBBBBB BB BBBBBBBB
+                                XXXX XXXXXXXXXXXX XXXXXX XXXXXXXXXXXXXXXXXX XXXXXXXXXXXXX
+                                    XXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXX
+                                        XXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                    XXXXXXXXX
+                                    XXXXXXX
+                                XXXXXX
+                            BBBBBB
+                        BBBBB
+                        XXXXXXXXXX
+                            BBB BBBBB BB BBBB
+                                XXXX XXXXXXXXXXXXXXXXX XXXXXXXBB BBBBBBBBBBBBBB XXXXXXXXX BBBBB XXXXXXXXXXXXXX
+                                    XXXX XXXXXXXXXXXXXXXXXX
+                                        BB BBBBBBBBBBBBBBBBBBBB
+                                            XXXXXX XXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                        BBBB
+                                            XXXXXX XXXXXX XXXXXXX XXXXXXXXXXXXXXXXXXXXXXX
+                                        BBBBB
+                                        BB BBBBBBBBBBBBB BB BBBBBBBBBBBBB
+                                            XXXXXX XXXXXXXXXXXXXXXXXXX XXXXXXXXX XXXXXXXXXXXXXXXX XXXXXXXXXXXXXXX XXXXXXXXXXXX
+                                            XXXX XXXXXXXXXXXXXXXXXXXX
+                                                XXXXXX XXXXXXXXXXX XXXXXXXXXXX XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXX gettext(u'Choose your file...') XX
+                                                XXXXX XXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXX
+                                                    XXXXXXX XXXXXXXXXXXXX XXXXXXXXXX XXXXXXX XXXXXXXXXXXXXX
+                                                        XX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                                    XXXXXXXXX
+                                                XXXXXXX
+                                            XXXXXX
+                                            XXXX XXXXXXXXXXXXXXXXX
+                                                XX XXXXXXX XXXXXXXXXXXXXXXX gettext(u'See current file') XXXX
+                                            XXXXXX
+                                        BBBB
+                                            BBBBBBBBBBBB BBBBB BBBBBBBBBBBBBBBBBBB BBBBBBBBB
+                                            XXXXX XXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXX
+                                        BBBBB
+                                    XXXXXX
+                                        
+                                    BB BBBBBBBBBBBB
+                                        XXXX XXXXXXXXXXXX XXXXXXXXXXXX XXXXXXXXXXXXXXXXX XXXXXXXXX XXXXXXXXXXXXX
+                                            XXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXX
+                                                XXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                            XXXXXXXXX
+                                            XXXX
+                                                BBB BBBBB BB BBBBBBBBBBBB
+                                                    XXXXXXXXX
+                                                BBBBBB
+                                            XXXXX
+                                        XXXXXX
+                                    BBBBB
+                                XXXXXX
+                            BBBBBB
+
+                            XXXX XXXXXXXXXXXXXXXXXXX
+                                XXXX XXXXXXXXXXXXXXXX XXXXXXXXXXXXX
+                                    XXXXXXXXX XXXX XXXX XXXXXXXXX XXXXXX XXXXXXXXXXXX
+                                XXXXXX
+                            XXXXXX
+
+                            XXXX XXXXXXXXXXXXXXXXXXX
+                                XXXX XXXXXXXXXXXXXXXXXX
+                                     XXXXXXX XXXXXXXXXXXXX XXXXXXXXXX XXXXXXXXXX XXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXX gettext(u'Close') XXXXXXXXX
+                                    XXXXXXX XXXXXXXXXX XXXXXXXXXX XXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                                XXXXXX
+                            XXXXXX
+                        XXXXXXXXXXX
+                    XXXXXXX
+                XXXX XXXX XXXX XXX
+              XXXXXX
+            XXXXXX
+        XXXXXX
+    XXXXXX
+XXXXXX
+
+BBBBB BBBBBBBBBBB
+    
+    
+    XXXXXXX XXXXXXXXXXXXXXXXXXXXXXX
+      XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX X
+            XXX XXXX X XXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            XXXXXXXX
+                XXXX XBBB BBBBBBBBBBBBBBBBBBBBBBBBB BBBBBBBBBXX
+                XXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                XXXXX XXXXX
+                XXXXXX XXXXXX
+                XXXXXXXXXXXX XXXXXX
+                XXXXXXXXXXXX XXXXXX
+                XXXXXXXX XXXXXXXXXXXXXX X
+                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                XX
+                XXXXXX XXXXXXXXXXXXXXX
+                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                X
+            XXX
+            XXXXXXXXXXXXXXXXXXXXXXX
+          XXX
+  XXXXXXXXX
+BBBBBBBB BBBBBBBBBBB
+XXXX XXXXXXXX XXX
