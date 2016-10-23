4a8492e8c20eba48da4e787443a0e31eaac47e13
fbormann <felipe.bormann@gmail.com>
2016-10-23 14:48:53 -0300

    modified notification card template, removed login notifications and set up new messages

--- app/views.py
+++ app/views.py
@@ -40,7 +40,6 @@ class AppIndex(LoginRequiredMixin, LogMixin, ListView, NotificationMixin):
 			else:
 				self.template_name = "home_teacher_student_content.html"
 			
-		super(AppIndex, self).createNotification("you have just logged in", resource_name="home", resource_link="/")
 		return self.response_class(request = self.request, template = self.template_name, context = context, using = self.template_engine, **response_kwargs)
 
 
