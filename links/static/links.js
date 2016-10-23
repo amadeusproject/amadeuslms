e14046bdb935ce4ba7a5c5723c0817fa2251a45a
Gustavo Bernardo <Gustavo Bernardo>
2016-10-23 16:35:40 -0300

    Fixing js [Issue:#126]

--- links/static/links.js
+++ links/static/links.js
@@ -1,8 +1,7 @@
 function get_modal_link(url, id,div_content){
     $.get(url, function (data) {
-        //alert(data);
-        // $(div_content).empty();
-        // $(div_content).append(data);
+        $(div_content).empty();
+        $(div_content).append(data);
         $(id).modal('show');
     });
 
