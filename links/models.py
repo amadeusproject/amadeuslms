4d6c7ca533a33bbff750163878f9f18b0c9896ee
Gustavo Bernardo <Gustavo Bernardo>
2016-10-28 13:13:07 -0300

    Included link image properly [Issue:#119]

--- links/models.py
+++ links/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from courses.models import Material
 from autoslug.fields import AutoSlugField
+from django.contrib.staticfiles.templatetags.staticfiles import static
 # Create your models here.
 class Link(Material):
 	link_url = models.URLField()
 	link_description = models.CharField(max_length=200)
+	image = models.ImageField(upload_to = 'links/',blank = True)
 	class Meta:
 		verbose_name = 'Link'
 		verbose_name_plural = "Links"
