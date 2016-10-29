55daf89d0c27acb4fb389c3bd950c65018c50ff5
Gustavo Bernardo <Gustavo Bernardo>
2016-10-28 22:19:10 -0300

    Link validation included on updatelink form [Issue:#122]

--- links/forms.py
+++ links/forms.py
@@ -21,6 +21,15 @@ class CreateLinkForm(forms.ModelForm):
         fields = ['name','link_url','link_description']
 
 class UpdateLinkForm(forms.ModelForm):
+    def clean_link_url(self):
+        link_url = self.cleaned_data['link_url']
+        try:
+            response = requests.head(link_url)
+            if response.status_code >= 400:
+                raise forms.ValidationError(_('Invalid url!'))
+        except requests.ConnectionError:
+            raise forms.ValidationError(_('Invalid url!'))
+        return link_url
     class Meta:
         model = Link
         fields = ['name','link_url','link_description']
