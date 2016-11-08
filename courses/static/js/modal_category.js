7db0fa5b43452bfa5da431d82dfcc0df5ed93b60
Jailson Dias <jcd2@cin.ufpe.br>
2016-11-08 19:35:44 -0300

    ajeitando o bug de quando o professor clica em editar tópico #234

--- courses/static/js/modal_category.js
+++ courses/static/js/modal_category.js
@@ -16,16 +16,16 @@ var Submite = {
   }
 }
 
-var modal = {
-  get: function (url, id_modal, id_div_modal){
-    $.get(url, function(data){
-      if($(id_modal).length){
-        $(id_div_modal).empty();
-        $(id_div_modal).append(data);
-      } else {
-        $(id_div_modal).append(data);
-      }
-      $(id_modal).modal('show');
-    });
-  }
-};
+// var modal = {
+//   get: function (url, id_modal, id_div_modal){
+//     $.get(url, function(data){
+//       if($(id_modal).length){
+//         $(id_div_modal).empty();
+//         $(id_div_modal).append(data);
+//       } else {
+//         $(id_div_modal).append(data);
+//       }
+//       $(id_modal).modal('show');
+//     });
+//   }
+// };
