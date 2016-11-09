5b91c656a337aa707d80be5aab484dc7e35481f2
fbormann <felipe.bormann@gmail.com>
2016-11-09 01:41:33 -0300

    added pagination configuration for API

--- amadeus/settings.py
+++ amadeus/settings.py
@@ -215,7 +215,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',),
     'DEFAULT_PERMISSION_CLASSES':(
-        'rest_framework.permissions.IsAuthenticated',)
+        'rest_framework.permissions.IsAuthenticated',),
+     'PAGE_SIZE': 10, #pagination purposes
 }
 
 OAUTH2_PROVIDER = {
