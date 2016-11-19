e175a38faae3d96fec85851abafabf461aaeb56b
Gustavo Bernardo <Gustavo Bernardo>
2016-11-19 10:34:30 -0300

    Fixed page's title [Issue:#416]

--- core/views.py
+++ core/views.py
@@ -112,6 +112,7 @@ class GuestView (ListView):
 	def get_context_data (self, **kwargs):
 		context = super(GuestView, self).get_context_data(**kwargs)
 		context['categorys_courses'] = CourseCategory.objects.all()
+		context['title'] = _("Guest")
 		return context
 
 
