44d74d2353bc0f81f4ceb81a63d848060463b8c1
Zambom <erikzambom@hotmail.com>
2016-12-19 15:54:41 -0200

    Fixing settings to run admin

--- amadeus/settings.py
+++ amadeus/settings.py
@@ -86,9 +86,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                'core.context_processors.notifications',
-                'courses.context_processors.courses',
             ],
         },
     },
