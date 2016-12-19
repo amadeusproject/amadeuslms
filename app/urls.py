d25c5790e02beeb2e009ecec0627f0aa81b9f16d
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2016-12-19 13:12:02 -0300

    erasing all other apps besides users one

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.AppIndex.as_view(), name='index'),
-    url(r'^settings/(?P<page>[\w_-]+)/$', views.AmadeusSettings.as_view(), name='settings'),
-]
--- core/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Log
-
-class LogSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Log
-		fields = '__all__'
-		 
