4418a020c40f5f36473c423116b304de4566c900
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2016-12-21 16:03:44 -0300

    added unauth user test case for index url

--- courses/tests/test_views.py
+++ courses/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, RequestFactory
 from users.models import User
-
+from django.contrib.auth.models import AnonymousUser
 from .. import views
 
 class Index_Test(TestCase):
@@ -17,3 +17,13 @@ class Index_Test(TestCase):
 		response = views.IndexView.as_view()(request)
 		
 		self.assertEqual(response.status_code, 200)
+
+	def test_index_get_unauth(self):
+
+		request = self.factory.get('courses/')
+
+		request.user = AnonymousUser()
+
+		response = views.IndexView.as_view()(request)
+		
+		self.assertEqual(response.status_code, 302) #Which means it is been redirected to login page
