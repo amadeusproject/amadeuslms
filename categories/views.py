8dfa451e35435a39e3e23c0ee6d7717b314681e4
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2016-12-23 17:12:00 -0300

    added list categories on index and icons for notifications on cards

--- categories/views.py
+++ categories/views.py
@@ -16,7 +16,7 @@ class IndexView(LoginRequiredMixin, ListView):
     login_url = reverse_lazy("users:login")
     redirect_field_name = 'next'
     queryset = Category.objects.all()
-    template_name = 'categories/home.html'
+    template_name = 'categories/list.html'
     context_object_name = 'categories'
 
 
@@ -30,15 +30,15 @@ class IndexView(LoginRequiredMixin, ListView):
         if self.request.user.is_staff:
             context['page_template'] = "categories/home_admin_content.html"
         else:
-            context['page_template'] = "categories/home_teacher_student_content.html"
+            context['page_template'] = "categories/home_teacher_student.html"
 
         context['title'] = _('Home')
 
         if self.request.is_ajax():
             if self.request.user.is_staff:
-                self.template_name = "home_admin_content.html"
+                self.template_name = "categories/home_admin_content.html"
             else:
-                self.template_name = "home_teacher_student_content.html"
+                self.template_name = "categories/home_teacher_student_content.html"
 
         return self.response_class(request = self.request, template = self.template_name, context = context, using = self.template_engine, **response_kwargs)
 
@@ -46,19 +46,19 @@ class IndexView(LoginRequiredMixin, ListView):
         context = super(IndexView, self).get_context_data(**kwargs)
         list_categories = None
         if has_role(self.request.user,'system_admin'):
-            list_categories = self.get_queryset().order_by('name')
-            # categorys_categories = CourseCategory.objects.all()
+            categories = self.get_queryset().order_by('name')
+            
         elif has_role(self.request.user,'professor'):
         	pass
             #list_categories = self.get_queryset().filter(professors__in = [self.request.user]).order_by('name')
             # categorys_categories = CourseCategory.objects.filter(course_category__professors__name = self.request.user.name).distinct()
         elif has_role(self.request.user, 'student'):
-        	pass
-            #list_categories = self.get_queryset().filter(students__in = [self.request.user]).order_by('name')
+            categories = self.get_queryset().order_by('name')
 
         
         context['title'] = _('Categories')
-       
+        context['categories'] = categories
+
         return context
 
 class CreateCategory(HasRoleMixin, CreateView):
