4924a5361725c83b0edfde1b720aa023bfbeb446
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2016-12-26 13:58:48 -0300

    changed category model

--- categories/forms.py
+++ categories/forms.py
@@ -4,8 +4,5 @@ from .models import Category
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ('category_father', 'name', 'description', 'visible', 'coordinators', )
-        widgets = {
-			'category_father': forms.Select(),
-			
-		}
+        fields = ( 'name', 'description', 'visible', 'coordinators', )
+       
