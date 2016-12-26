32fa886a5042971851a76cff06a99ec284430336
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2016-12-26 17:21:52 -0300

    modified subject app, added migration

--- subjects/models.py
+++ subjects/models.py
@@ -10,25 +10,25 @@ from categories.models import Category
 
 class Subject(models.Model):
 
-	name = models.CharField( _("Name"), unique = True,max_length= 200)
-	slug = AutoSlugField(_("Slug"),populate_from='name',unique=True)
+    name = models.CharField( _("Name"), unique = True,max_length= 200)
+    slug = AutoSlugField(_("Slug"),populate_from='name',unique=True)
 
-	description_brief = models.CharField(_("simpler_description"), max_length= 100)
-	description = models.CharField(_("description"), max_length = 300)
-	visible = models.BooleanField(_("visible"))
+    description_brief = models.CharField(_("simpler_description"), max_length= 100)
+    description = models.CharField(_("description"), max_length = 300)
+    visible = models.BooleanField(_("visible"))
 
-   	init_date = models.DateField(_('Begin of Subject Date'))
-	end_date = models.DateField(_('End of Subject Date'))
+    init_date = models.DateField(_('Begin of Subject Date'))
+    end_date = models.DateField(_('End of Subject Date'))
 
-	create_date = models.DateTimeField(_('Creation Date'), auto_now_add = True)
-	update_date = models.DateTimeField(_('Date of last update'), auto_now=True)
+    create_date = models.DateTimeField(_('Creation Date'), auto_now_add = True)
+    update_date = models.DateTimeField(_('Date of last update'), auto_now=True)
 
-	professor = models.ForeignKey(User, related_name="professor")
-	students = models.ManyToManyField(User,verbose_name=_('Students'), related_name='subject_student', blank = True)
+    professor = models.ForeignKey(User, related_name="professor")
+    students = models.ManyToManyField(User,verbose_name=_('Students'), related_name='subject_student', blank = True)
 
-	category = models.ForeignKey(Category, related_name="subject_category")
+    category = models.ForeignKey(Category, related_name="subject_category")
 
-	max_upload_size = models.IntegerField(_("Maximum upload size"))
+    max_upload_size = models.IntegerField(_("Maximum upload size"))
     class Meta:
         verbose_name = "Subject"
         verbose_name_plural = "Subjects"
@@ -37,4 +37,4 @@ class Subject(models.Model):
         pass
     
 
-class Marker(models.Model):
+
