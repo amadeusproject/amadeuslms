1979c41be7f3d305a2af7ca89303390cea15a212
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2017-01-10 10:25:30 -0300

    added delete modal template

--- subjects/urls.py
+++ subjects/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 	url(r'^create/(?P<slug>[\w_-]+)/$', views.SubjectCreateView.as_view(), name='create'),
 	url(r'^replicate/(?P<subject_slug>[\w_-]+)/$', views.SubjectCreateView.as_view(), name='replicate'),
 	url(r'^update/(?P<slug>[\w_-]+)/$', views.SubjectUpdateView.as_view(), name='update'),
+	url(r'^delete/(?P<slug>[\w_-]+)/$', views.SubjectDeleteView.as_view(), name='delete'),
 	#url(r'^modal/(?P<subject_slug>[\w_-]+)/$', views.subscribe_subject(), name='subscribe'),
 ]
--- subjects/views.py
+++ subjects/views.py
@@ -203,6 +203,32 @@ class SubjectUpdateView(LogMixin, UpdateView):
         return reverse_lazy('subjects:index')
 
 
+
+class SubjectDeleteView(LoginRequiredMixin, HasRoleMixin, LogMixin, DeleteView):
+   
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
+    model = Subject
+    template_name = 'subject/delete.html'
+
+    def dispatch(self, *args, **kwargs):
+        subject = get_object_or_404(Subject, slug = self.kwargs.get('slug'))
+        
+        return super(SubjectDeleteView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(SubjectDeleteView, self).get_context_data(**kwargs)
+       
+        return context
+
+    def get_success_url(self):
+        
+       
+
+        #super(DeleteSubjectView, self).createLog(self.request.user, self.log_component, self.log_action, self.log_resource, self.log_context)
+
+        return reverse_lazy('subjects:index')
+
 def subscribe_subject(request, subject_slug):
     subject = get_object_or_404(Subject, slug= subject_slug)
     subject.students.add(request.user)
