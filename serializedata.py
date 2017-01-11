30aba4890c42f1ef02be14f717055d73c4233746
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2017-01-11 11:12:23 -0300

    solved coordinators problem on initial page

--- serializedata.py
+++ serializedata.py
@@ -1,10 +1,18 @@
 from django.core import serializers
 import os
-
+import json
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amadeus.settings")
 
 from subjects.models import Subject
+from users.models import User
+from categories.models import Category
+
+subject_data = serializers.serialize("json", Subject.objects.all())
 
 
-data = serializers.serialize("json", Subject.objects.all())
-print(data)
+users_data = serializers.serialize("json", User.objects.all())
+
+category_data = serializers.serialize("json", Category.objects.all())
+
+with open('data.txt', 'w') as outfile:
+    json.dump(category_data, outfile)
