6ac8d81389e834d681879a200e3e2ae67fb4fdfa
Zambom <erikzambom@hotmail.com>
2017-01-12 18:23:18 -0200

    Change css templatetag by context_processor

--- amadeus/settings.py
+++ amadeus/settings.py
@@ -92,6 +92,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'amadeus.context_processors.theme',
             ],
         },
     },
