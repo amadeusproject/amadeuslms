e33fbf30a4697b23867a3cd728122a1475d6701f
Zambom <erikzambom@hotmail.com>
2017-01-17 18:44:51 -0200

    Adding breadcrumb topic view update

--- amadeus/static/js/topics.js
+++ amadeus/static/js/topics.js
@@ -1,3 +1,30 @@
+$('.collapse').on('shown.bs.collapse', function (e) {
+    if($(this).is(e.target)){
+    	var li = $(".breadcrumb").find('li:last-child');
+    	var li_text = $(li).html();
+    	var url = $(".subs_url").val();
+    	var new_li = $(li).clone();
+    	
+    	new_li.html($(this).parent().find('.panel-title').text());
+
+    	$(li).html("<a href='" + url + "'>" + li_text + "</a>");
+    	$(li).append("<span class='divider'>/</span>");
+
+    	new_li.appendTo('.breadcrumb');
+    }
+});
+
+$('.collapse').on('hide.bs.collapse', function (e) {
+    if($(this).is(e.target)){
+    	$(".breadcrumb").find('li:last-child').remove();
+
+    	var li = $(".breadcrumb").find('li:last-child');
+    	var text = $(li).find('a').text();
+
+    	$(li).html(text);
+	}
+});
+
 // utilizado para fazer a re-organização dos tópicos
 $("#topics-accordion").sortable({ 
     delay: 100,
