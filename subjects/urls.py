c16ffd7db6d3ac17bcc8b28cb56a6cfce72f45ea
Zambom <erikzambom@hotmail.com>
2017-01-20 17:13:11 -0200

    Changing datepicker plugin and removing some unnecessary files

--- subjects/urls.py
+++ subjects/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
 	url(r'^home/$', views.HomeView.as_view(), name='home'),
 	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^category/(?P<slug>[\w_-]+)/$', views.IndexView.as_view(), name='cat_view'),
-	url(r'^(?P<option>[\w_-]+)/$', views.IndexView.as_view(), name='index'),
 	url(r'^create/(?P<slug>[\w_-]+)/$', views.SubjectCreateView.as_view(), name='create'),
 	url(r'^replicate/(?P<subject_slug>[\w_-]+)/$', views.SubjectCreateView.as_view(), name='replicate'),
 	url(r'^update/(?P<slug>[\w_-]+)/$', views.SubjectUpdateView.as_view(), name='update'),
@@ -13,4 +12,5 @@ urlpatterns = [
 	url(r'^view/(?P<slug>[\w_-]+)/$', views.SubjectDetailView.as_view(), name='view'),
 	url(r'^subscribe/(?P<slug>[\w_-]+)/$', views.SubjectSubscribeView.as_view(), name='subscribe'),
 	url(r'^search/$', views.SubjectSearchView.as_view(), name='search'),
+	url(r'^(?P<option>[\w_-]+)/$', views.IndexView.as_view(), name='index'),
 ]
