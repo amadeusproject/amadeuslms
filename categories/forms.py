a3b64ac69771503718f2cb7ca41b7b3be4fff2bb
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2017-01-24 11:44:41 -0300

    create category is working fine

--- categories/forms.py
+++ categories/forms.py
@@ -13,9 +13,9 @@ class CategoryForm(forms.ModelForm):
     def clean_name(self):
         name = self.cleaned_data.get('name')
         if self.instance.id:
-            same_name = Subject.objects.filter(name__unaccent__iexact = name).exclude(id = self.instance.id)
+            same_name = Category.objects.filter(name__unaccent__iexact = name).exclude(id = self.instance.id)
         else:
-            same_name = Subject.objects.filter(name__unaccent__iexact = name)
+            same_name = Category.objects.filter(name__unaccent__iexact = name)
 
         if same_name.count() > 0:
             self._errors['name'] = [_('There is another category with this name, try another one.')]
