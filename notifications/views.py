a406516b0ef3ba62d4810629a40415a6c423b3c6
Zambom <erikzambom@hotmail.com>
2017-01-30 17:21:04 -0200

    Adding subject notification history

--- notifications/views.py
+++ notifications/views.py
@@ -20,6 +20,7 @@ class SubjectNotifications(LoginRequiredMixin, generic.ListView):
 	context_object_name = 'notifications'
 	template_name = 'notifications/subject.html'
 	paginate_by = 10
+	total = 0
 
 	def dispatch(self, request, *args, **kwargs):
 		slug = self.kwargs.get('slug', '')
@@ -36,6 +37,8 @@ class SubjectNotifications(LoginRequiredMixin, generic.ListView):
 
 		notifications = Notification.objects.filter(user = self.request.user, task__resource__topic__subject = subject, creation_date = datetime.now())
 
+		self.total = notifications.count()
+
 		return notifications
 
 	def get_context_data(self, **kwargs):
@@ -46,5 +49,50 @@ class SubjectNotifications(LoginRequiredMixin, generic.ListView):
 
 		context['title'] = _('%s - Pendencies')%(subject.name)
 		context['subject'] = subject
+		context['total'] = self.total
+
+		return context
+
+class SubjectHistory(LoginRequiredMixin, generic.ListView):
+	login_url = reverse_lazy("users:login")
+	redirect_field_name = 'next'
+
+	context_object_name = 'notifications'
+	template_name = 'notifications/subject.html'
+	paginate_by = 10
+	total = 0
+	num_rows = 0
+
+	def dispatch(self, request, *args, **kwargs):
+		slug = self.kwargs.get('slug', '')
+		subject = get_object_or_404(Subject, slug = slug)
+
+		if not has_subject_view_permissions(request.user, subject):
+			return redirect(reverse_lazy('subjects:home'))
+
+		return super(SubjectHistory, self).dispatch(request, *args, **kwargs)
+
+	def get_queryset(self):
+		slug = self.kwargs.get('slug', '')
+		subject = get_object_or_404(Subject, slug = slug)
+
+		notifications = Notification.objects.filter(user = self.request.user, task__resource__topic__subject = subject).order_by("-creation_date")
+
+		self.total = notifications.filter(creation_date = datetime.now()).count()
+		self.num_rows = notifications.count()
+
+		return notifications
+
+	def get_context_data(self, **kwargs):
+		context = super(SubjectHistory, self).get_context_data(**kwargs)
+
+		slug = self.kwargs.get('slug', '')
+		subject = get_object_or_404(Subject, slug = slug)
+
+		context['title'] = _('%s - Pendencies')%(subject.name)
+		context['subject'] = subject
+		context['history'] = True
+		context['total'] = self.total
+		context['rows'] = self.num_rows
 
 		return context
