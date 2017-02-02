be2f58b756f0d1b9a6a0306463ebb9094662bb3f
Zambom <erikzambom@hotmail.com>
2017-02-02 18:29:25 -0200

    Disabled inactivity count when running video

--- amadeus/static/js/ytvideo.js
+++ amadeus/static/js/ytvideo.js
@@ -3,29 +3,33 @@ var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
+var keepAlive;
 	// Replace the 'ytplayer' element with an <iframe> and
 	// YouTube player after the API code downloads.
 var player;
 function onYouTubePlayerAPIReady() {
 	player = new YT.Player('video');
 
-	player.addEventListener("onStateChange", "onPlayerStateChange");
-	
+	player.addEventListener("onStateChange", "onPlayerStateChange");	
 }
 
 function onPlayerStateChange(event) {
-		switch (event.data) {
+	switch (event.data) {
 		case YT.PlayerState.UNSTARTED:
 	  		console.log('unstarted');
 	  		break;
 		case YT.PlayerState.ENDED:
+			clearInterval(keepAlive)
 	  		console.log('ended');
 	  		break;
 		case YT.PlayerState.PLAYING:
+			keepAlive = setInterval(function () {
+				keepLogged()
+			}, 60000);
 	  		console.log('playing');
 	  		break;
 		case YT.PlayerState.PAUSED:
+			clearInterval(keepAlive)
 	  		console.log('paused');
 	  		break;
 		case YT.PlayerState.BUFFERING:
@@ -35,4 +39,8 @@ function onPlayerStateChange(event) {
 	  		console.log('video cued');
 	  	break;
   	}
+}
+
+function keepLogged() {
+	$(document).mousemove();
 }
