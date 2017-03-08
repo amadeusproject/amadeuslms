e9e66d9cc131358c1bc5902c1c209637d6884215
fbormann <felipe.bormann@gmail.com>
2017-03-08 00:00:39 -0300

    choice field now has initial data and support topics, still has to configure optional fields and submit form

--- reports/forms.py
+++ reports/forms.py
@@ -2,11 +2,23 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 import datetime
 
+
+
 class CreateInteractionReportForm(forms.Form):
-	topics = forms.ChoiceField(required=True)
+	topic = forms.ChoiceField(required=True, label= _("topics to select data from"))
 	init_date = forms.DateField(required=True)
 	end_date = forms.DateField(required=True)
 
 	from_mural = forms.BooleanField()
 	from_messages = forms.BooleanField()
 
+	class Meta:
+		fields = ('topic', 'init_date', 'end_date', 'from_mural' , 'from_messages')
+
+	def __init__(self, *args, **kwargs):
+		super(CreateInteractionReportForm, self).__init__(*args, **kwargs)
+		
+		initial = kwargs['initial']
+		topics = list(initial['topic'])
+		
+		self.fields['topic'].choices = [(topic.id, topic.name) for topic in topics]
