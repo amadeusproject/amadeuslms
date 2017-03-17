d99b5261421eaf38601af991edf5b8c2b7672456
Jailson Dias <jcd2@cin.ufpe.br>
2017-03-17 00:43:02 -0300

    Criando página para a video conferencia e conectando-a com o servidor do jitsi

--- webconference/urls.py
+++ webconference/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -9,4 +10,5 @@ urlpatterns = [
 	url(r'^delete/(?P<slug>[\w_-]+)/$', views.DeleteView.as_view(), name = 'delete'),
 	url(r'^window_view/(?P<slug>[\w_-]+)/$', views.NewWindowView.as_view(), name = 'window_view'),
 	url(r'^view/(?P<slug>[\w_-]+)/$', views.InsideView.as_view(), name = 'view'),
+	url(r'^conference/(?P<slug>[\w_-]+)/$',views.Conference.as_view(), name = 'conference'),
 ]
