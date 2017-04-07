64e4ac3a3b8dd906777ec6a71a77c548e8a0ff1b
Gustavo Bernardo <Gustavo Bernardo>
2017-04-06 22:12:39 -0300

    News views updated, started manage and create views

--- news/views.py
+++ news/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from log.models import Log
+from log.mixins import LogMixin
+from django.core.urlresolvers import reverse, reverse_lazy
 
-# Create your views here.
+from .models import News
+from .forms import NewsForm
+
+class ListNewsView(LoginRequiredMixin,LogMixin,generic.ListView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
+
+    template_name = 'news/list.html'
+    context_object_name = "news"
+    paginate_by = 10
+
+    def get_queryset(self):
+        news = News.objects.all()
+        return news
+
+class CreateNewsView(LoginRequiredMixin,LogMixin,generic.edit.CreateView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
+
+
+    template_name = 'news/_form.html'
+    form_class = NewsForm
+
+    def form_invalid(self, form):
+    	context = super(CreateNewsView, self).form_invalid(form)
+    	context.status_code = 400
+
+    	return context
