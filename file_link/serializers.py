25c98d9bfd110d2530250411bf6090d768d9645d
Zambom <erikzambom@hotmail.com>
2017-05-02 23:09:07 -0300

    Adding resource pendencies and the possibility to include users data in subject backup

--- file_link/serializers.py
+++ file_link/serializers.py
@@ -2,13 +2,26 @@ from rest_framework import serializers
 
 from subjects.serializers import TagSerializer
 from topics.serializers import TopicSerializer
+from pendencies.serializers import PendenciesSerializer
+from students_group.serializers import StudentsGroupSerializer
 
 from .models import FileLink
 
 class SimpleFileLinkSerializer(serializers.ModelSerializer):
 	topic = TopicSerializer()
 	tags = TagSerializer(many = True)
+	pendencies_resource = PendenciesSerializer(many = True)
 
 	class Meta:
 		model = FileLink
 		exclude = ('students', 'groups',)
+
+class CompleteFileLinkSerializer(serializers.ModelSerializer):
+	topic = TopicSerializer()
+	tags = TagSerializer(many = True)
+	pendencies_resource = PendenciesSerializer(many = True)
+	groups = StudentsGroupSerializer(many = True)
+
+	class Meta:
+		model = FileLink
+		fields = '__all__'
