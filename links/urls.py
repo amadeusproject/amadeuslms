b7b7e74522ed3ac46147b1fd0535d72094cbe226
Jailson Dias <jcd2@cin.ufpe.br>
2017-05-05 20:00:53 -0300

    criando a página de relatórios para links

--- links/urls.py
+++ links/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
 	url(r'^create/(?P<slug>[\w_-]+)/$', views.CreateLinkView.as_view(), name='create'),
 	url(r'^delete/(?P<slug>[\w_-]+)/$', views.DeleteLinkView.as_view(), name='delete'),
 	url(r'^update/(?P<topic_slug>[\w_-]+)/(?P<slug>[\w_-]+)/$', views.UpdateLinkView.as_view(), name='update'),
-	url(r'^view/(?P<slug>[\w_-]+)/$', views.RedirectUrl.as_view(), name='view')]
+	url(r'^view/(?P<slug>[\w_-]+)/$', views.RedirectUrl.as_view(), name='view'),
+    url(r'^chart/(?P<slug>[\w_-]+)/$', views.StatisticsView.as_view(), name = 'get_chart'),
+    url(r'^send-message/(?P<slug>[\w_-]+)/$', views.SendMessage.as_view(), name = 'send_message'),
+]
