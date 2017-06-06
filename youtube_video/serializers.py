80ca0cbd035edf6d37e5b147e60d6c8f77d51474
Zambom <erikzambom@hotmail.com>
2017-06-05 23:42:43 -0300

    Restoring simple backup working (minor adjusts to be made)

--- youtube_video/serializers.py
+++ youtube_video/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 
 from subjects.serializers import TagSerializer
 from topics.serializers import TopicSerializer
 from pendencies.serializers import PendenciesSerializer
 from students_group.serializers import StudentsGroupSerializer
 from users.serializers import UserBackupSerializer
+
+from subjects.models import Tag
+from topics.models import Topic, Resource
+from pendencies.models import Pendencies
 
 from .models import YTVideo
 
@@ -21,6 +26,62 @@ class SimpleYTVideoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = YTVideo
 		exclude = ('students', 'groups',)
+
+	def create(self, data):
+		topic = data['topic']
+
+		ytvideo = None
+
+		if not topic["id"] is None:
+			if topic['id'] == "":
+				topic_exist = Topic.objects.filter(subject = topic['subject'], name__unaccent__iexact = topic["name"])
+
+				if topic_exist.exists():
+					topic = topic_exist[0]
+				else:
+					topic = Topic.objects.create(name = topic['name'], subject = topic['subject'], repository = topic['repository'], visible = topic['visible'], order = topic['order'])
+				
+				data["topic"] = topic
+			else:
+				data["topic"] = get_object_or_404(Topic, id = topic["id"])
+			
+
+			ytvideo_data = data
+			
+			pendencies = ytvideo_data["pendencies_resource"]
+			del ytvideo_data["pendencies_resource"]
+
+			ytvideo = YTVideo()
+			ytvideo.name = ytvideo_data["name"]
+			ytvideo.brief_description = ytvideo_data["brief_description"]
+			ytvideo.show_window = ytvideo_data["show_window"]
+			ytvideo.all_students = ytvideo_data["all_students"]
+			ytvideo.visible = ytvideo_data["visible"]
+			ytvideo.order = ytvideo_data["order"]
+			ytvideo.topic = ytvideo_data["topic"]
+			ytvideo.url = ytvideo_data["url"]
+
+			ytvideo.save()
+			
+			tags = data["tags"]
+
+			for tag in tags:
+				if tag["id"] == "":
+					tag = Tag.objects.create(name = tag["name"])
+				else:
+					tag = get_object_or_404(Tag, id = tag["id"])
+
+				ytvideo.tags.add(tag)
+				
+			resource = get_object_or_404(Resource, id = ytvideo.id)
+
+			for pend in pendencies:
+				Pendencies.objects.create(resource = resource, **pend)
+
+		return ytvideo
+
+	def update(self, instance, data):
+		return instance
 
 class CompleteYTVideoSerializer(serializers.ModelSerializer):
 	topic = TopicSerializer()
