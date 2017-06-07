5e2e0c8fb80eb15a8b394466726f00cb18d4324c
Zambom <erikzambom@hotmail.com>
2017-06-07 00:13:32 -0300

    Start writing validationand save scripts for complete restore

--- pdf_file/serializers.py
+++ pdf_file/serializers.py
@@ -123,12 +123,102 @@ class SimplePDFFileSerializer(serializers.ModelSerializer):
 
 class CompletePDFFileSerializer(serializers.ModelSerializer):
 	file = serializers.CharField(required = False, allow_blank = True)
-	topic = TopicSerializer()
+	topic = TopicSerializer('get_subject')
 	tags = TagSerializer(many = True)
 	pendencies_resource = PendenciesSerializer(many = True)
 	groups = StudentsGroupSerializer(many = True)
 	students = UserBackupSerializer(many = True)
 
+	def get_subject(self, obj):
+		subject = self.context.get("subject", None)
+
+		return subject
+
+	def validate(self, data):
+		files = self.context.get('files', None)
+
+		if files:
+			file_path = os.path.join(settings.MEDIA_ROOT, data["file"])
+
+			if os.path.isfile(file_path):
+				dst_path = os.path.join(settings.MEDIA_ROOT, "tmp")
+
+				path = files.extract(data["file"], dst_path)
+
+				new_name = "files/file_" + str(time.time()) + os.path.splitext(data["file"])[1]
+
+				os.rename(os.path.join(dst_path, path), os.path.join(settings.MEDIA_ROOT, new_name))
+				
+				data["file"] = new_name
+			else:
+				path = files.extract(data["file"], settings.MEDIA_ROOT)
+
+		return data
+
 	class Meta:
 		model = PDFFile
 		fields = '__all__'
+
+	def create(self, data):
+		topic = data['topic']
+
+		pdf = None
+
+		if not topic["id"] is None:
+			if "subject" in topic:
+				r_exits = Resource.objects.filter(topic__subject = topic["subject"], name__unaccent__iexact = data["name"])
+			else:
+				r_exits = Resource.objects.filter(topic__subject__id = topic["subject_id"], name__unaccent__iexact = data["name"])
+
+			if not r_exits.exists():
+				if topic['id'] == "":
+					topic_exist = Topic.objects.filter(subject = topic['subject'], name__unaccent__iexact = topic["name"])
+
+					if topic_exist.exists():
+						topic = topic_exist[0]
+					else:
+						topic = Topic.objects.create(name = topic['name'], subject = topic['subject'], repository = topic['repository'], visible = topic['visible'], order = topic['order'])
+					
+					data["topic"] = topic
+				else:
+					data["topic"] = get_object_or_404(Topic, id = topic["id"])
+
+
+				f = open(os.path.join(settings.MEDIA_ROOT, data["file"]), encoding="latin-1")
+				file = File(f)
+
+				data["file"] = file
+
+				pdf_data = data
+				
+				pendencies = pdf_data["pendencies_resource"]
+				del pdf_data["pendencies_resource"]
+
+				pdf = PDFFile()
+				pdf.name = pdf_data["name"]
+				pdf.brief_description = pdf_data["brief_description"]
+				pdf.show_window = pdf_data["show_window"]
+				pdf.all_students = pdf_data["all_students"]
+				pdf.visible = pdf_data["visible"]
+				pdf.order = pdf_data["order"]
+				pdf.topic = pdf_data["topic"]
+				pdf.file = pdf_data["file"]
+
+				pdf.save()
+
+				tags = data["tags"]
+
+				for tag in tags:
+					if tag["id"] == "":
+						tag = Tag.objects.create(name = tag["name"])
+					else:
+						tag = get_object_or_404(Tag, id = tag["id"])
+
+					pdf.tags.add(tag)
+				
+				resource = get_object_or_404(Resource, id = pdf.id)
+
+				for pend in pendencies:
+					Pendencies.objects.create(resource = resource, **pend)
+
+		return pdf
--- youtube_video/serializers.py
+++ youtube_video/serializers.py
@@ -90,12 +90,76 @@ class SimpleYTVideoSerializer(serializers.ModelSerializer):
 		return instance
 
 class CompleteYTVideoSerializer(serializers.ModelSerializer):
-	topic = TopicSerializer()
+	topic = TopicSerializer('get_subject')
 	tags = TagSerializer(many = True)
 	pendencies_resource = PendenciesSerializer(many = True)
 	groups = StudentsGroupSerializer(many = True)
 	students = UserBackupSerializer(many = True)
 
+	def get_subject(self, obj):
+		subject = self.context.get("subject", None)
+
+		return subject
+
 	class Meta:
 		model = YTVideo
 		fields = '__all__'
+
+	def create(self, data):
+		topic = data['topic']
+
+		ytvideo = None
+
+		if not topic["id"] is None:
+			if "subject" in topic:
+				r_exits = Resource.objects.filter(topic__subject = topic["subject"], name__unaccent__iexact = data["name"])
+			else:
+				r_exits = Resource.objects.filter(topic__subject__id = topic["subject_id"], name__unaccent__iexact = data["name"])
+
+			if not r_exits.exists():
+				if topic['id'] == "":
+					topic_exist = Topic.objects.filter(subject = topic['subject'], name__unaccent__iexact = topic["name"])
+
+					if topic_exist.exists():
+						topic = topic_exist[0]
+					else:
+						topic = Topic.objects.create(name = topic['name'], subject = topic['subject'], repository = topic['repository'], visible = topic['visible'], order = topic['order'])
+					
+					data["topic"] = topic
+				else:
+					data["topic"] = get_object_or_404(Topic, id = topic["id"])
+				
+
+				ytvideo_data = data
+				
+				pendencies = ytvideo_data["pendencies_resource"]
+				del ytvideo_data["pendencies_resource"]
+
+				ytvideo = YTVideo()
+				ytvideo.name = ytvideo_data["name"]
+				ytvideo.brief_description = ytvideo_data["brief_description"]
+				ytvideo.show_window = ytvideo_data["show_window"]
+				ytvideo.all_students = ytvideo_data["all_students"]
+				ytvideo.visible = ytvideo_data["visible"]
+				ytvideo.order = ytvideo_data["order"]
+				ytvideo.topic = ytvideo_data["topic"]
+				ytvideo.url = ytvideo_data["url"]
+
+				ytvideo.save()
+				
+				tags = data["tags"]
+
+				for tag in tags:
+					if tag["id"] == "":
+						tag = Tag.objects.create(name = tag["name"])
+					else:
+						tag = get_object_or_404(Tag, id = tag["id"])
+
+					ytvideo.tags.add(tag)
+					
+				resource = get_object_or_404(Resource, id = ytvideo.id)
+
+				for pend in pendencies:
+					Pendencies.objects.create(resource = resource, **pend)
+
+		return ytvideo
