a6449796c672dcd68eb8fcd246a53e314c93ecb7
Zambom <erikzambom@hotmail.com>
2017-09-11 22:53:18 -0300

    Adding mural visualizations to django admin

--- mural/admin.py
+++ mural/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import MuralVisualizations
 # Register your models here.
+
+class MuralAdmin(admin.ModelAdmin):
+	list_display = ['user', 'viewed', 'post', 'comment', 'date_viewed']
+
+admin.site.register(MuralVisualizations, MuralAdmin)
