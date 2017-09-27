69144042dc03637b2b98e0e22407db0ff8716b10
Zambom <erikzambom@hotmail.com>
2017-09-26 23:15:17 -0300

    Adjusting subject mural post with resource visualization permissions

--- mural/utils.py
+++ mural/utils.py
@@ -1,5 +1,7 @@
 from django.db.models import Q
 
+from .models import SubjectPost
+
 from users.models import User
 
 def getSpaceUsers(user, post):
@@ -20,3 +22,49 @@ def getSpaceUsers(user, post):
 			return User.objects.filter(Q(is_staff = True) | Q(professors__id = space) | Q(coordinators__subject_category__id = space) | Q(subject_student__id = space)).exclude(id = user).distinct()
 
 	return None
+
+def getSubjectPosts(subject, user, favorites, mines):
+	if not favorites:
+		if mines:
+			if not user.is_staff:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(
+					Q(space__id = subject) & Q(mural_ptr__user = user) & (
+					Q(space__category__coordinators = user) | 
+					Q(space__professor = user) | 
+					Q(resource__isnull = True) |
+					(Q(resource__isnull = False) & (Q(resource__all_students = True) | Q(resource__students = user) | Q(resource__groups__participants = user))))).distinct()
+			else:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(space__id = subject, mural_ptr__user = user)
+		else:
+			if not user.is_staff:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(
+					Q(space__id = subject) & (
+					Q(space__category__coordinators = user) | 
+					Q(space__professor = user) | 
+					Q(resource__isnull = True) |
+					(Q(resource__isnull = False) & (Q(resource__all_students = True) | Q(resource__students = user) | Q(resource__groups__participants = user))))).distinct()
+			else:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(space__id = subject)
+	else:
+		if mines:
+			if not user.is_staff:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(
+					Q(space__id = subject) & Q(favorites_post__isnull = False) & Q(favorites_post__user = user) & Q(mural_ptr__user = user) & (
+					Q(space__category__coordinators = user) | 
+					Q(space__professor = user) | 
+					Q(resource__isnull = True) |
+					(Q(resource__isnull = False) & (Q(resource__all_students = True) | Q(resource__students = user) | Q(resource__groups__participants = user))))).distinct()
+			else:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(space__id = subject, favorites_post__isnull = False, favorites_post__user = user, mural_ptr__user = user)
+		else:
+			if not user.is_staff:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(
+					Q(space__id = subject) & Q(favorites_post__isnull = False) & Q(favorites_post__user = user) & (
+					Q(space__category__coordinators = user) | 
+					Q(space__professor = user) | 
+					Q(resource__isnull = True) |
+					(Q(resource__isnull = False) & (Q(resource__all_students = True) | Q(resource__students = user) | Q(resource__groups__participants = user))))).distinct()
+			else:
+				posts = SubjectPost.objects.extra(select = {"most_recent": "greatest(mural_mural.last_update, (select max(mural_comment.last_update) from mural_comment where mural_comment.post_id = mural_subjectpost.mural_ptr_id))"}).filter(space__id = subject, favorites_post__isnull = False, favorites_post__user = user)
+
+	return posts
