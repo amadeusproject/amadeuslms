00160d3a5cd32aa28140cbfc56937ae87cc0ef22
vitor-amartins <vmam@cin.ufpe.br>
2017-10-10 01:07:01 -0300

    trying to get favorite messages to app

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from .models import TalkMessages
+from django.db.models import Q
+
+from .models import TalkMessages, ChatFavorites
 
 from subjects.serializers import SubjectSerializer
 from users.serializers import UserSerializer
@@ -8,7 +10,16 @@ from users.serializers import UserSerializer
 class ChatSerializer(serializers.ModelSerializer):
 	user = UserSerializer()
 	subject = SubjectSerializer()
+	favorite = serializers.SerializerMethodField()
+
+	def get_favorite(self, message):
+		user = self.context.get("request_user", None)
+
+		if not user is None:
+			return ChatFavorites.objects.filter(Q(user__email = user) & Q(message = message)).exists()
+
+		return False
 
 	class Meta:
 		model = TalkMessages
-		fields = ('text', 'user', 'subject', 'image_url', 'create_date', )
+		fields = ('text', 'user', 'subject', 'image_url', 'create_date', 'favorite')
