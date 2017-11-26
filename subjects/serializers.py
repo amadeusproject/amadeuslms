5afaa67585516e456bf43cc02dfd8ede2814ce03
fbormann <felipe.bormann@gmail.com>
2017-11-26 14:24:28 -0300

    refactor(serializers): remove whitespace and indentation

--- subjects/serializers.py
+++ subjects/serializers.py
@@ -19,38 +19,44 @@ from chat.models import ChatVisualizations
 
 from .models import Subject, Tag
 
-class TagSerializer(serializers.ModelSerializer):
-	def validate(self, data):
-		query = Tag.objects.filter(name = data['name'])
-		
-		if query.exists():
-			data['id'] = query[0].id
-		else:
-			data['id'] = ""
 
-		return data
-	
-	class Meta:
-		model = Tag
-		fields = '__all__'
-		extra_kwargs = {
-        	"name": {
-	            "validators": [],
-	        },
-	    }
-		validators = []
+class TagSerializer(serializers.ModelSerializer):
+    def validate(self, data):
+        query = Tag.objects.filter(name=data['name'])
+
+        if query.exists():
+            data['id'] = query[0].id
+        else:
+            data['id'] = ""
+
+        return data
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
+        extra_kwargs = {
+            "name": {
+                "validators": [],
+            },
+        }
+        validators = []
+
 
 class SubjectSerializer(serializers.ModelSerializer):
-	notifications = serializers.SerializerMethodField()
+    notifications = serializers.SerializerMethodField()
 
-	def get_notifications(self, subject):
-		user = self.context.get("request_user", None)
+    def get_notifications(self, subject):
+        user = self.context.get("request_user", None)
 
-		if not user is None:
-			return ChatVisualizations.objects.filter(Q(user = user) & Q(viewed = False) & Q(message__subject = subject) & (Q(user__is_staff = True) | Q(message__subject__students = user) | Q(message__subject__professor = user) | Q(message__subject__category__coordinators = user))).distinct().count()
-			
-		return 0
-	
-	class Meta:
-		model = Subject
-		fields = ["name", "slug", "visible", "description_brief", "description", "notifications"]
+        if user is not None:
+            return ChatVisualizations.objects.filter(Q(user=user) & Q(viewed=False) & Q(message__subject=subject)
+                                                     & (Q(user__is_staff=True) | Q(message__subject__students=user)
+                                                        | Q(message__subject__professor=user)
+                                                        | Q(message__subject__category__coordinators=user)))\
+                .distinct().count()
+
+        return 0
+
+    class Meta:
+        model = Subject
+        fields = ["name", "slug", "visible", "description_brief", "description", "notifications"]
