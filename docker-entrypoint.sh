b2f099b91c37fa300a6fc17078c86ecec3134ff4
fbormann <felipe.bormann@gmail.com>
2018-10-07 14:09:26 -0300

    update code (REMEMBER TO SPLIT THIS COMMIT)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-
-python3 manage.py migrate --noinput
-python3 manage.py runserver 0.0.0.0:8000
+python manage.py makemigrations
+#python3 manage.py makemigrations
+#python3 manage.py migrate --noinput
+#python3 initadmin.py
+#python3 manage.py runserver 0.0.0.0:8000
