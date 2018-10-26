d587acc662f2112fea89a543aac89003710ca9f2
fbormann <felipe.bormann@gmail.com>
2018-10-25 22:48:06 -0300

    refactor(docker-entrypoint.sh): add initadmin.py call and remove makemigrations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-python3 manage.py makemigrations
-python3 manage.py migrate --noinput
-#python3 initadmin.py
+python3 manage.py migrate 
 python3 manage.py compilemessages
 python3 manage.py runserver 0.0.0.0:8000
