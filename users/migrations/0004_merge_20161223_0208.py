39a672ef58d3a1a3e2b0eac7169e401fc3315c8c
fbormann <felipe.bormann@gmail.com>
2018-10-27 14:54:37 -0300

    feat(users/migrations): add admin user migration

--- users/migrations/0004_merge_20161223_0208.py
+++ users/migrations/0004_merge_20161223_0208.py
@@ -3,7 +3,16 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
+from django.contrib.auth import get_user_model
 
+def create_admin_user(apps, schema_editor):
+    User = apps.get_model("users", "User")
+    db_alias = schema_editor.connection.alias
+    new_admin_user = get_user_model().objects.create_user( email="admin@amadeus.com", 
+        password="admin",
+        username="admin", 
+        last_name="admin", 
+        is_staff= True)
 
 class Migration(migrations.Migration):
 
@@ -13,4 +22,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(create_admin_user, migrations.RunPython.noop),
     ]
