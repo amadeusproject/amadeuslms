1df167d0494aa69fad1c8ffa9f584c49433233f0
fbormann <felipe.bormann@gmail.com>
2019-01-27 18:02:50 -0200

    refactor(docker-entrypoint.sh): fix collectstatic and docker container boot up is now running again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# update the staticfiles directory
+python3 manage.py collectstatic --no-input
 python3 manage.py migrate 
 python3 manage.py compilemessages
-python3 manage.py runserver 0.0.0.0:8000
+gunicorn --bind :8000 amadeus.wsgi:application
+# python3 manage.py runserver 0.0.0.0:8000
