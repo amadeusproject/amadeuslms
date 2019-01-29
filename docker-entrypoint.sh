0b9d15c4b171e0c05da768abc004c0a53333743b
fbormann <felipe.bormann@gmail.com>
2019-01-28 23:08:00 -0200

    refactor(docker-entrypoint.sh): add makemigrations command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # update the staticfiles directory
 python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations
 python3 manage.py migrate 
 python3 manage.py compilemessages
 gunicorn --bind :8000 amadeus.wsgi:application
