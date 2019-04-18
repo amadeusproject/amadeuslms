8a4e73ecc38cbc8ca76c9f370ca3e9e55d4d0a31
fbormann <felipe.bormann@gmail.com>
2019-04-17 22:19:03 -0300

    refactor(log/mixins.py): remove context of mixin

--- log/mixins.py
+++ log/mixins.py
@@ -10,44 +10,36 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-
-import json
-
 from .models import Log
 
-from users.models import User
 
 class LogMixin(object):
-	log_component = None
-	log_context = None
-	log_action = None
-	log_resource = None
+    log_component = None
+    log_action = None
+    log_resource = None
 
-	def createLog(self, actor = None, component = '', log_action = '', log_resource = '', context = {}):
-		if actor.is_authenticated:
-			log = Log()
-			log.user = str(actor)
-			log.user_id = actor.id
-			log.user_email = actor.email
-			if self.log_context is not None:
-				log.context = self.log_context
-			else:
-				log.context = context
-			if self.log_component is not None:
-				log.component = self.log_component
-			else:
-				log.component = component
+    def create_log(self, actor=None, component='', log_action='', log_resource=''):
+        if actor.is_authenticated:
+            log = Log()
+            log.user = str(actor)
+            log.user_id = actor.id
+            log.user_email = actor.email
 
-			if self.log_action is not None:
-				log.action = self.log_action
-			else:
-				log.action = log_action
-			if self.log_resource is not None:
-				log.resource = self.log_resource
-			else:
-				log.resource = log_resource
+            if self.log_component is not None:
+                log.component = self.log_component
+            else:
+                log.component = component
 
-			log.save()
+            if self.log_action is not None:
+                log.action = self.log_action
+            else:
+                log.action = log_action
+            if self.log_resource is not None:
+                log.resource = self.log_resource
+            else:
+                log.resource = log_resource
 
-	def dispatch(self, request, *args, **kwargs):
-		return super(LogMixin, self).dispatch(request, *args, **kwargs)
+            log.save()
+
+    def dispatch(self, request, *args, **kwargs):
+        return super(LogMixin, self).dispatch(request, *args, **kwargs)
