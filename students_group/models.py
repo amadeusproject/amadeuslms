a965f46786244e02ed1ce48e8acbd55a2884f3da
fbormann <felipe.bormann@gmail.com>
2019-04-17 23:08:49 -0300

    refactor(students_group/models.py): format code and add on_delete clause

--- students_group/models.py
+++ students_group/models.py
@@ -10,26 +10,29 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-from django.db import models
 from autoslug.fields import AutoSlugField
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from subjects.models import Subject
 from users.models import User
 
+
 class StudentsGroup(models.Model):
-	name = models.CharField(_('Name'), max_length = 200)
-	slug = AutoSlugField(_("Slug"), populate_from = 'name', unique = True)
-	description = models.TextField(_('Description'), blank = True)
-	subject = models.ForeignKey(Subject, verbose_name = _('Subject'), related_name = 'group_subject', null = True)
-	participants = models.ManyToManyField(User, verbose_name = _('Participants'), related_name = 'group_participants', blank = True)
-	create_date = models.DateTimeField(_('Create Date'), auto_now_add = True)
-	last_update = models.DateTimeField(_('Last Update'), auto_now = True)
+    name = models.CharField(_('Name'), max_length=200)
+    slug = AutoSlugField(_("Slug"), populate_from='name', unique=True)
+    description = models.TextField(_('Description'), blank=True)
+    subject = models.ForeignKey(Subject, verbose_name=_('Subject'), related_name='group_subject',
+                                null=True, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, verbose_name=_('Participants'),
+                                          related_name='group_participants', blank=True)
+    create_date = models.DateTimeField(_('Create Date'), auto_now_add=True)
+    last_update = models.DateTimeField(_('Last Update'), auto_now=True)
 
-	class Meta:
-		verbose_name = _('Students Group')
-		verbose_name_plural = _('Students Groups')
-		ordering = ['name']
+    class Meta:
+        verbose_name = _('Students Group')
+        verbose_name_plural = _('Students Groups')
+        ordering = ['name']
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
