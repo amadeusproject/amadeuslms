9ed233cce552fad2ab2946a9e17042b7010c9be0
fbormann <felipe.bormann@gmail.com>
2019-04-17 22:09:09 -0300

    refactor(log/models.py): remove context json field

--- log/models.py
+++ log/models.py
@@ -10,24 +10,22 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 from django.utils.translation import ugettext_lazy as _
 
+
 class Log(models.Model):
-	component = models.TextField(_('Component (Module / App)'))
-	context = JSONField(_('Context'), blank = True)
-	action = models.TextField(_('Action'))
-	resource = models.TextField(_('Resource'))
-	user = models.CharField(_('Actor'), max_length = 100)
-	user_id = models.IntegerField(_('Actor id'))
-	user_email = models.EmailField(_('Actor Mail'))
-	datetime = models.DateTimeField(_("Date and Time of action"), auto_now_add = True)
+    component = models.TextField(_('Component (Module / App)'))
+    action = models.TextField(_('Action'))
+    resource = models.TextField(_('Resource'))
+    user = models.CharField(_('Actor'), max_length=100)
+    user_id = models.IntegerField(_('Actor id'))
+    user_email = models.EmailField(_('Actor Mail'))
+    datetime = models.DateTimeField(_("Date and Time of action"), auto_now_add=True)
 
-	class Meta:
-		verbose_name = _('Log')
-		verbose_name_plural = _('Logs')
+    class Meta:
+        verbose_name = _('Log')
+        verbose_name_plural = _('Logs')
 
-	def __str__(self):
-		return str(self.user) + ' / ' + self.component
+    def __str__(self):
+        return str(self.user) + ' / ' + self.component
