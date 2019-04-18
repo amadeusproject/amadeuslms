7fcb34cd0ad53db9a7e67939bb45129b82c221d3
fbormann <felipe.bormann@gmail.com>
2019-04-17 22:56:24 -0300

    refactor(subjects/models.py): format code, remove unused imports and add on_cascade clause on category field of subject model

--- subjects/models.py
+++ subjects/models.py
@@ -10,33 +10,28 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
+from autoslug.fields import AutoSlugField
 from django.db import models
 from django.db.models import Q
-
-from autoslug.fields import AutoSlugField
-
 from django.utils.translation import ugettext_lazy as _
 
+from categories.models import Category
 from users.models import User
 
-from django.core.exceptions import ValidationError
-
-from categories.models import Category
-import datetime
 
 class Tag(models.Model):
-    name = models.CharField( _("Name"), unique = True,max_length= 200, blank = True)
-    
+    name = models.CharField(_("Name"), unique=True, max_length=200, blank=True)
+
     def __str__(self):
         return self.name
 
-class Subject(models.Model):
 
-    name = models.CharField( _("Name"), unique = False, max_length= 200)
-    slug = AutoSlugField(_("Slug"),populate_from='name',unique=True)
+class Subject(models.Model):
+    name = models.CharField(_("Name"), unique=False, max_length=200)
+    slug = AutoSlugField(_("Slug"), populate_from='name', unique=True)
 
     description_brief = models.TextField(_("simpler description"), blank=True)
-    description = models.TextField(_("description"), blank= True)
+    description = models.TextField(_("description"), blank=True)
     visible = models.BooleanField(_("visible"))
 
     init_date = models.DateField(_('Begin of Subject Date'))
@@ -44,19 +39,21 @@ class Subject(models.Model):
 
     tags = models.ManyToManyField(Tag, verbose_name='tags', blank=True)
 
-    create_date = models.DateTimeField(_('Creation Date'), auto_now_add = True)
+    create_date = models.DateTimeField(_('Creation Date'), auto_now_add=True)
     update_date = models.DateTimeField(_('Date of last update'), auto_now=True)
 
     subscribe_begin = models.DateField(_('Begin Subscribe'))
     subscribe_end = models.DateField(_('End Subscribe'))
 
     professor = models.ManyToManyField(User, related_name="professors", blank=True)
-    students = models.ManyToManyField(User,verbose_name=_('Students'), related_name='subject_student', blank = True)
+    students = models.ManyToManyField(User, verbose_name=_('Students'),
+                                      related_name='subject_student', blank=True)
 
-    category = models.ForeignKey(Category, related_name="subject_category", null=True)
+    category = models.ForeignKey(Category, related_name="subject_category", null=True,
+                                 on_delete="CASCADE")
 
     max_upload_size = models.IntegerField(_("Maximum upload size"), default=1024, null=True)
-    
+
     class Meta:
         verbose_name = "Subject"
         verbose_name_plural = "Subjects"
@@ -64,12 +61,11 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def get_participants(self):
         data = User.objects.filter(
-            Q(is_staff = True) | Q(subject_student__slug = self.slug) |
-            Q(professors__slug = self.slug) |
-            Q(coordinators__subject_category__slug = self.slug)
-            ).distinct()
+            Q(is_staff=True) | Q(subject_student__slug=self.slug) | Q(professors__slug=self.slug) |
+            Q(coordinators__subject_category__slug=self.slug)) \
+            .distinct()
 
         return data
