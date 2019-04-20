4b7be87a90d40576148a3b8a0ef0167f696b4970
fbormann <felipe.bormann@gmail.com>
2019-04-20 18:54:16 -0300

    refactor(amadeus/urls.py): change urls list to have app_name field on include

--- amadeus/urls.py
+++ amadeus/urls.py
@@ -19,42 +19,41 @@ from rest_framework.documentation import include_docs_urls
 
 from .views import index
 
-
 urlpatterns = [
-    url(r'^users/', include('users.urls', namespace = 'users')),
+    url(r'^users/', include(('users.urls', 'users'), namespace='users')),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name = 'home'),
-    url(r'^api/', include('api.urls', namespace = 'api')),
-    url(r'^categories/', include('categories.urls', namespace = 'categories')),
-    url(r'^subjects/', include('subjects.urls', namespace = 'subjects')),
-    url(r'^groups/', include('students_group.urls', namespace = 'groups')),
-    url(r'^topics/', include('topics.urls', namespace = 'topics')),
-    url(r'^chat/', include('chat.urls', namespace = 'chat')),
-    url(r'^mural/', include('mural.urls', namespace = 'mural')),
-    url(r'^webpages/', include('webpage.urls', namespace = 'webpages')),
-    url(r'^ytvideo/', include('youtube_video.urls', namespace = 'youtube')),
-    url(r'^file_links/', include('file_link.urls', namespace = 'file_links')),
-    url(r'^goals/', include('goals.urls', namespace = 'goals')),
-    url(r'^mailsender/', include('mailsender.urls', namespace = 'mailsender')),
-    url(r'^security/', include('security.urls', namespace = 'security')),
-    url(r'^themes/', include('themes.urls', namespace = 'themes')),
-    url(r'^pendencies/', include('notifications.urls', namespace = 'notifications')),
+    url(r'^$', index, name='home'),
+    url(r'^api/', include(('api.urls', 'api'), namespace='api')),
+    url(r'^categories/', include(('categories.urls', 'categories'), namespace='categories')),
+    url(r'^subjects/', include(('subjects.urls', 'subjects'), namespace='subjects')),
+    url(r'^groups/', include(('students_group.urls', 'students_group'), namespace='groups')),
+    url(r'^topics/', include(('topics.urls', 'topics'), namespace='topics')),
+    url(r'^chat/', include(('chat.urls', 'chat'), namespace='chat')),
+    url(r'^mural/', include(('mural.urls', 'mural'), namespace='mural')),
+    url(r'^webpages/', include(('webpage.urls', 'webpage'), namespace='webpages')),
+    url(r'^ytvideo/', include(('youtube_video.urls', 'youtube_video'), namespace='youtube')),
+    url(r'^file_links/', include(('file_link.urls', 'file_link'), namespace='file_links')),
+    url(r'^goals/', include('goals.urls', namespace='goals')),
+    url(r'^mailsender/', include('mailsender.urls', namespace='mailsender')),
+    url(r'^security/', include('security.urls', namespace='security')),
+    url(r'^themes/', include('themes.urls', namespace='themes')),
+    url(r'^pendencies/', include('notifications.urls', namespace='notifications')),
     url(r'^links/', include('links.urls', namespace='links')),
     url(r'^pdf_files/', include('pdf_file.urls', namespace='pdf_files')),
     url(r'^questionary/', include('questionary.urls', namespace='questionary')),
-    url(r'^webconferences/', include('webconference.urls', namespace = 'webconferences')),
+    url(r'^webconferences/', include('webconference.urls', namespace='webconferences')),
     url(r'^news/', include('news.urls', namespace='news')),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^analytics/', include('analytics.urls', namespace='analytics')),
     url(r'^dashboards/', include('dashboards.urls', namespace='dashboards')),
     url(r'^bulletin/', include('bulletin.urls', namespace='bulletin')),
     url(r'^questions_database/', include('banco_questoes.urls', namespace='questions_database')),
-    url(r'^api-docs/', include_docs_urls(title = 'REST Api Documentation')),
-    #API
+    url(r'^api-docs/', include_docs_urls(title='REST Api Documentation')),
+    # API
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^summernote/', include('django_summernote.urls')),
     url(r'session_security/', include('session_security.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
