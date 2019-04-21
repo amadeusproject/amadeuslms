67f5271087ccac00862c63fe0c77b996121b2000
fbormann <felipe.bormann@gmail.com>
2019-04-21 18:37:26 -0300

    refactor(themses/views.py): update from django.core.urlresolvers to django.urls and format code

--- themes/views.py
+++ themes/views.py
@@ -10,89 +10,93 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-from django.views import generic
-from django.shortcuts import render
-from django.contrib import messages
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.utils.translation import ugettext_lazy as _
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect,HttpRequest
-from django.shortcuts import redirect
-from log.decorators import log_decorator
 from braces import views as braces_mixins
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext_lazy as _
+from django.views import generic
 
-from .models import Themes
+from log.decorators import log_decorator
 from .forms import BasicElemetsForm, CSSStyleForm
-
-class IndexView(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin, generic.TemplateView):
-	login_url = reverse_lazy("users:login")
-	redirect_field_name = 'next'
-
-	template_name = 'themes/index.html'
-
-	def get_context_data(self, **kwargs):
-		context = super(IndexView, self).get_context_data(**kwargs)
-
-		context['title'] = _('Themes')
-		context['settings_menu_active'] = "settings_menu_active"
-
-		return context
-
-class BasicElementsSettings(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin, generic.UpdateView):
-	login_url = reverse_lazy("users:login")
-	redirect_field_name = 'next'
-
-	template_name = 'themes/basic_update.html'
-	model = Themes
-	form_class = BasicElemetsForm
-	success_url = reverse_lazy("subjects:home")
-
-	def get_object(self, queryset = None):
-		return Themes.objects.get(id = 1)
-
-	def form_valid(self, form):
-		form.save()
-
-		messages.success(self.request, _("Theme settings updated successfully!"))
-
-		return super(BasicElementsSettings, self).form_valid(form)
-
-	def get_context_data(self, **kwargs):
-		context = super(BasicElementsSettings, self).get_context_data(**kwargs)
-
-		context['title'] = _('Basic Elements')
-		context['settings_menu_active'] = "settings_menu_active"
-
-		return context
+from .models import Themes
 
 
-class CSSStyleSettings(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin, generic.UpdateView):
-	login_url = reverse_lazy("users:login")
-	redirect_field_name = 'next'
+class IndexView(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin,
+                generic.TemplateView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
 
-	template_name = 'themes/css_update.html'
-	model = Themes
-	form_class = CSSStyleForm
-	success_url = reverse_lazy("subjects:home")
+    template_name = 'themes/index.html'
 
-	def get_object(self, queryset = None):
-		return Themes.objects.get(id = 1)
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
 
-	def form_valid(self, form):
-		form.save()
+        context['title'] = _('Themes')
+        context['settings_menu_active'] = "settings_menu_active"
 
-		messages.success(self.request, _("Theme settings updated successfully!"))
+        return context
 
-		return super(CSSStyleSettings, self).form_valid(form)
 
-	def get_context_data(self, **kwargs):
-		context = super(CSSStyleSettings, self).get_context_data(**kwargs)
+class BasicElementsSettings(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin,
+                            generic.UpdateView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
 
-		context['title'] = _('CSS Selector')
-		context['settings_menu_active'] = "settings_menu_active"
+    template_name = 'themes/basic_update.html'
+    model = Themes
+    form_class = BasicElemetsForm
+    success_url = reverse_lazy("subjects:home")
 
-		return context
+    def get_object(self, queryset=None):
+        return Themes.objects.get(id=1)
+
+    def form_valid(self, form):
+        form.save()
+
+        messages.success(self.request, _("Theme settings updated successfully!"))
+
+        return super(BasicElementsSettings, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(BasicElementsSettings, self).get_context_data(**kwargs)
+
+        context['title'] = _('Basic Elements')
+        context['settings_menu_active'] = "settings_menu_active"
+
+        return context
+
+
+class CSSStyleSettings(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin,
+                       generic.UpdateView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
+
+    template_name = 'themes/css_update.html'
+    model = Themes
+    form_class = CSSStyleForm
+    success_url = reverse_lazy("subjects:home")
+
+    def get_object(self, queryset=None):
+        return Themes.objects.get(id=1)
+
+    def form_valid(self, form):
+        form.save()
+
+        messages.success(self.request, _("Theme settings updated successfully!"))
+
+        return super(CSSStyleSettings, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(CSSStyleSettings, self).get_context_data(**kwargs)
+
+        context['title'] = _('CSS Selector')
+        context['settings_menu_active'] = "settings_menu_active"
+
+        return context
+
 
 @log_decorator("contrast", "click", "contrast")
 def Contrast(request):
-	caminho = request.META.get('HTTP_REFERER')
-	return HttpResponseRedirect(caminho)
+    caminho = request.META.get('HTTP_REFERER')
+    return HttpResponseRedirect(caminho)
