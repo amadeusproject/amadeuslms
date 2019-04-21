f874b49da702df5e210931d3fbfe8eb54c0d5799
fbormann <felipe.bormann@gmail.com>
2019-04-21 17:04:44 -0300

    refactor(file_link/forms.py): fix imports and resubmit widget

--- file_link/forms.py
+++ file_link/forms.py
@@ -13,107 +13,110 @@ Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título
 # coding=utf-8
 from django import forms
 from django.utils.translation import ugettext_lazy as _
-from django.utils.html import strip_tags
-from resubmit.widgets import ResubmitFileWidget
 
-from subjects.models import Tag
 from subjects.forms import ParticipantsMultipleChoiceField
-
+from subjects.models import Tag
 from .models import FileLink
 
+
 class FileLinkForm(forms.ModelForm):
-	subject = None
-	MAX_UPLOAD_SIZE = 10*1024*1024
+    subject = None
+    MAX_UPLOAD_SIZE = 10 * 1024 * 1024
 
-	students = ParticipantsMultipleChoiceField(queryset = None, required = False)
-	
-	def __init__(self, *args, **kwargs):
-		super(FileLinkForm, self).__init__(*args, **kwargs)
+    students = ParticipantsMultipleChoiceField(queryset=None, required=False)
 
-		self.subject = kwargs['initial'].get('subject', None)
-		
-		if self.instance.id:
-			self.subject = self.instance.topic.subject
-			self.initial['tags'] = ", ".join(self.instance.tags.all().values_list("name", flat = True))
-		
-		self.fields['students'].queryset = self.subject.students.all()
-		self.fields['groups'].queryset = self.subject.group_subject.all()
+    def __init__(self, *args, **kwargs):
+        super(FileLinkForm, self).__init__(*args, **kwargs)
 
-	tags = forms.CharField(label = _('Tags'), required = False)
+        self.subject = kwargs['initial'].get('subject', None)
 
-	class Meta:
-		model = FileLink
-		fields = ['name', 'file_content', 'brief_description', 'all_students', 'students', 'groups', 'visible']
-		labels = {
-			'name': _('File name'),
-		}
-		widgets = {
-			'brief_description': forms.Textarea,
-			'students': forms.SelectMultiple,
-			'groups': forms.SelectMultiple,
-			'file_content': ResubmitFileWidget(attrs={'accept':'image/jpeg,image/x-citrix-jpeg,image/png,image/x-citrix-png,image/x-png,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.openxmlformats-officedocument.presentationml.slideshow,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.ms-excel,text/html,application/msword,application/vnd.oasis.opendocument.presentation,application/vnd.oasis.opendocument.spreadsheet,application/vnd.oasis.opendocument.text,application/pdf,application/vnd.ms-powerpoint'}),
-		}
+        if self.instance.id:
+            self.subject = self.instance.topic.subject
+            self.initial['tags'] = ", ".join(
+                self.instance.tags.all().values_list("name", flat=True))
 
-	def clean_name(self):
-		name = self.cleaned_data.get('name', '')
-		
-		topics = self.subject.topic_subject.all()
+        self.fields['students'].queryset = self.subject.students.all()
+        self.fields['groups'].queryset = self.subject.group_subject.all()
 
-		for topic in topics:
-			if self.instance.id:
-				same_name = topic.resource_topic.filter(name__unaccent__iexact = name).exclude(id = self.instance.id).count()
-			else:
-				same_name = topic.resource_topic.filter(name__unaccent__iexact = name).count()
-		
-			if same_name > 0:
-				self._errors['name'] = [_('This subject already has a file link with this name')]
+    tags = forms.CharField(label=_('Tags'), required=False)
 
-				return ValueError
+    class Meta:
+        model = FileLink
+        fields = ['name', 'file_content', 'brief_description', 'all_students', 'students', 'groups',
+                  'visible']
+        labels = {
+            'name': _('File name'),
+        }
+        widgets = {
+            'brief_description': forms.Textarea,
+            'students': forms.SelectMultiple,
+            'groups': forms.SelectMultiple,
+            'file_content': forms.FileInput(attrs={
+                'accept': 'image/jpeg,image/x-citrix-jpeg,image/png,image/x-citrix-png,image/x-png,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.openxmlformats-officedocument.presentationml.slideshow,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.ms-excel,text/html,application/msword,application/vnd.oasis.opendocument.presentation,application/vnd.oasis.opendocument.spreadsheet,application/vnd.oasis.opendocument.text,application/pdf,application/vnd.ms-powerpoint'}),
+        }
 
-		return name
+    def clean_name(self):
+        name = self.cleaned_data.get('name', '')
 
-	def clean_file_content(self):
-		file_content = self.cleaned_data.get('file_content', False)
-		
-		if file_content:
-			if hasattr(file_content, '_size'):
-				if file_content._size > self.MAX_UPLOAD_SIZE:
-					self._errors['file_content'] = [_("The file is too large. It should have less than 10MB.")]
+        topics = self.subject.topic_subject.all()
 
-					return ValueError
-		
-		elif not self.instance.pk:
-			self._errors['file_content'] = [_('This field is required.')]
+        for topic in topics:
+            if self.instance.id:
+                same_name = topic.resource_topic.filter(name__unaccent__iexact=name).exclude(
+                    id=self.instance.id).count()
+            else:
+                same_name = topic.resource_topic.filter(name__unaccent__iexact=name).count()
 
-			return ValueError
+            if same_name > 0:
+                self._errors['name'] = [_('This subject already has a file link with this name')]
 
-		return file_content
+                return ValueError
 
-	def save(self, commit = True):
-		super(FileLinkForm, self).save(commit = True)
+        return name
 
-		self.instance.save()
+    def clean_file_content(self):
+        file_content = self.cleaned_data.get('file_content', False)
 
-		previous_tags = self.instance.tags.all()
+        if file_content:
+            if hasattr(file_content, '_size'):
+                if file_content._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['file_content'] = [
+                        _("The file is too large. It should have less than 10MB.")]
 
-		tags = self.cleaned_data['tags'].split(",")
+                    return ValueError
 
-        #Excluding unwanted tags
-		for prev in previous_tags:
-			if not prev.name in tags:
-				self.instance.tags.remove(prev)
-        
-		for tag in tags:
-			tag = tag.strip()
+        elif not self.instance.pk:
+            self._errors['file_content'] = [_('This field is required.')]
 
-			exist = Tag.objects.filter(name = tag).exists()
+            return ValueError
 
-			if exist:
-				new_tag = Tag.objects.get(name = tag)
-			else:
-				new_tag = Tag.objects.create(name = tag)
+        return file_content
 
-			if not new_tag in self.instance.tags.all():
-				self.instance.tags.add(new_tag)
+    def save(self, commit=True):
+        super(FileLinkForm, self).save(commit=True)
 
-		return self.instance
+        self.instance.save()
+
+        previous_tags = self.instance.tags.all()
+
+        tags = self.cleaned_data['tags'].split(",")
+
+        # Excluding unwanted tags
+        for prev in previous_tags:
+            if not prev.name in tags:
+                self.instance.tags.remove(prev)
+
+        for tag in tags:
+            tag = tag.strip()
+
+            exist = Tag.objects.filter(name=tag).exists()
+
+            if exist:
+                new_tag = Tag.objects.get(name=tag)
+            else:
+                new_tag = Tag.objects.create(name=tag)
+
+            if not new_tag in self.instance.tags.all():
+                self.instance.tags.add(new_tag)
+
+        return self.instance
