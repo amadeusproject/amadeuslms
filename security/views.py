9ee10e815ae3e61ad83a2cf9775fd29803a6d3ee
fbormann <felipe.bormann@gmail.com>
2019-04-21 18:17:18 -0300

    refactor(security/views.py): remove unnecessary imports, fix indentation and fix django.urls import

--- security/views.py
+++ security/views.py
@@ -10,40 +10,40 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-from django.views import generic
-from django.shortcuts import render
-from django.contrib import messages
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.utils.translation import ugettext_lazy as _
-
 from braces import views as braces_mixins
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
+from django.views import generic
 
-from .models import Security
 from .forms import SecurityForm
+from .models import Security
 
-class SecuritySettings(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin, generic.UpdateView):
-	login_url = reverse_lazy("users:login")
-	redirect_field_name = 'next'
 
-	template_name = 'security/update.html'
-	model = Security
-	form_class = SecurityForm
-	success_url = reverse_lazy("subjects:home")
+class SecuritySettings(braces_mixins.LoginRequiredMixin, braces_mixins.StaffuserRequiredMixin,
+                       generic.UpdateView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = 'next'
 
-	def get_object(self, queryset = None):
-		return Security.objects.get(id = 1)
+    template_name = 'security/update.html'
+    model = Security
+    form_class = SecurityForm
+    success_url = reverse_lazy("subjects:home")
 
-	def form_valid(self, form):
-		form.save()
+    def get_object(self, queryset=None):
+        return Security.objects.get(id=1)
 
-		messages.success(self.request, _("Security settings updated successfully!"))
+    def form_valid(self, form):
+        form.save()
 
-		return super(SecuritySettings, self).form_valid(form)
+        messages.success(self.request, _("Security settings updated successfully!"))
 
-	def get_context_data(self, **kwargs):
-		context = super(SecuritySettings, self).get_context_data(**kwargs)
+        return super(SecuritySettings, self).form_valid(form)
 
-		context['title'] = _('Security')
-		context['settings_menu_active'] = "settings_menu_active"
+    def get_context_data(self, **kwargs):
+        context = super(SecuritySettings, self).get_context_data(**kwargs)
 
-		return context
+        context['title'] = _('Security')
+        context['settings_menu_active'] = "settings_menu_active"
+
+        return context
