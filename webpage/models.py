b8bc4634855b648e50069757e5f132b6c56cf0ec
fbormann <felipe.bormann@gmail.com>
2019-04-20 22:05:54 -0300

    refactor(webpage/models.py): reformat code and fix urls import

--- webpage/models.py
+++ webpage/models.py
@@ -10,33 +10,34 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
+from django.urls import reverse_lazy
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.core.urlresolvers import reverse_lazy
 
 from topics.models import Resource
 
+
 class Webpage(Resource):
-	content = models.TextField(_('Webpage Content'), blank = True)
+    content = models.TextField(_('Webpage Content'), blank=True)
 
-	class Meta:
-		verbose_name = _('WebPage')
-		verbose_name_plural = _('WebPages')
+    class Meta:
+        verbose_name = _('WebPage')
+        verbose_name_plural = _('WebPages')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def access_link(self):
-		if self.show_window:
-			return reverse_lazy('webpages:window_view', args = (), kwargs = {'slug': self.slug})
+    def access_link(self):
+        if self.show_window:
+            return reverse_lazy('webpages:window_view', args=(), kwargs={'slug': self.slug})
 
-		return reverse_lazy('webpages:view', args = (), kwargs = {'slug': self.slug})
+        return reverse_lazy('webpages:view', args=(), kwargs={'slug': self.slug})
 
-	def update_link(self):
-		return 'webpages:update'
+    def update_link(self):
+        return 'webpages:update'
 
-	def delete_link(self):
-		return 'webpages:delete'
+    def delete_link(self):
+        return 'webpages:delete'
 
-	def delete_message(self):
-		return _('Are you sure you want delete the webpage')
+    def delete_message(self):
+        return _('Are you sure you want delete the webpage')
