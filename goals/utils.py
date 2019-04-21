096b02e9afe03bf19ca8db9a1b092ae125e4041e
fbormann <felipe.bormann@gmail.com>
2019-04-21 17:38:09 -0300

    refactor(goals/utils.py): add channels 2.x code and remove unnecessary imports

--- goals/utils.py
+++ goals/utils.py
@@ -10,75 +10,77 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-
 import json
 import textwrap
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.db.models import Q
-from django.utils import timezone
-from django.utils import formats
-from django.utils.html import strip_tags
 from django.template.loader import render_to_string
-from django.core.urlresolvers import reverse
+from django.urls import reverse
+from django.utils import formats
+from django.utils import timezone
+from django.utils.html import strip_tags
 from django.utils.translation import ugettext_lazy as _
 
-from channels import Group
-
-from api.utils import sendChatPushNotification
-
+from api.utils import send_chat_push_notification
 from chat.models import Conversation, TalkMessages, ChatVisualizations
 from users.models import User
-
 from .models import GoalItem, MyGoals
 
+
 def set_goals():
-	specifications = GoalItem.objects.filter(goal__limit_submission_date__date = timezone.now())
-	entries = []
+    specifications = GoalItem.objects.filter(goal__limit_submission_date__date=timezone.now())
+    entries = []
 
-	for goal in specifications:
-		users = User.objects.filter(subject_student = goal.goal.topic.subject)
+    for goal in specifications:
+        users = User.objects.filter(subject_student=goal.goal.topic.subject)
 
-		for user in users:
-			if not MyGoals.objects.filter(user = user, item = goal).exists():
-				entries.append(MyGoals(user = user, item = goal, value = goal.ref_value))
+        for user in users:
+            if not MyGoals.objects.filter(user=user, item=goal).exists():
+                entries.append(MyGoals(user=user, item=goal, value=goal.ref_value))
 
-	MyGoals.objects.bulk_create(entries)
+    MyGoals.objects.bulk_create(entries)
+
 
 def brodcast_dificulties(request, message, subject):
-	msg = TalkMessages()
-	msg.text = message
-	msg.user = request.user
-	msg.subject = subject
+    msg = TalkMessages()
+    msg.text = message
+    msg.user = request.user
+    msg.subject = subject
 
-	simple_notify = textwrap.shorten(strip_tags(msg.text), width = 30, placeholder = "...")
-	
-	for p in subject.professor.all():
-		talks = Conversation.objects.filter((Q(user_one = request.user) & Q(user_two__email = p.email)) | (Q(user_two = request.user) & Q(user_one__email = p.email)))
-		
-		if talks.count() > 0:
-			msg.talk = talks[0]
-		else:
-			msg.talk = Conversation.objects.create(user_one = request.user, user_two = p)
+    simple_notify = textwrap.shorten(strip_tags(msg.text), width=30, placeholder="...")
+    channel_layer = get_channel_layer()
 
-		msg.save()
+    for p in subject.professor.all():
+        talks = Conversation.objects.filter(
+            (Q(user_one=request.user) & Q(user_two__email=p.email)) | (
+                    Q(user_two=request.user) & Q(user_one__email=p.email)))
 
-		notification = {
-			"type": "chat",
-			"subtype": subject.slug,
-			"space": "subject",
-			"user_icon": request.user.image_url,
-			"notify_title": str(request.user),
-			"simple_notify": simple_notify,
-			"view_url": reverse("chat:view_message", args = (msg.id, ), kwargs = {}),
-			"complete": render_to_string("chat/_message.html", {"talk_msg": msg}, request),
-			"container": "chat-" + str(request.user.id),
-			"last_date": _("Last message in %s")%(formats.date_format(msg.create_date, "SHORT_DATETIME_FORMAT"))
-		}
+        if talks.count() > 0:
+            msg.talk = talks[0]
+        else:
+            msg.talk = Conversation.objects.create(user_one=request.user, user_two=p)
 
-		notification = json.dumps(notification)
+        msg.save()
 
-		Group("user-%s" % p.id).send({'text': notification})
+        notification = {
+            "type": "chat",
+            "subtype": subject.slug,
+            "space": "subject",
+            "user_icon": request.user.image_url,
+            "notify_title": str(request.user),
+            "simple_notify": simple_notify,
+            "view_url": reverse("chat:view_message", args=(msg.id,), kwargs={}),
+            "complete": render_to_string("chat/_message.html", {"talk_msg": msg}, request),
+            "container": "chat-" + str(request.user.id),
+            "last_date": _("Last message in %s") % (
+                formats.date_format(msg.create_date, "SHORT_DATETIME_FORMAT"))
+        }
 
-		sendChatPushNotification(p, msg)
+        notification = json.dumps(notification)
+        async_to_sync(channel_layer.send)("user-%s" % p.id, {'text': notification})
 
-		ChatVisualizations.objects.create(viewed = False, message = msg, user = p)
+        send_chat_push_notification(p, msg)
 
+        ChatVisualizations.objects.create(viewed=False, message=msg, user=p)
