eb260102ccb21f0799b3299eb5cc0c36656381dd
fbormann <felipe.bormann@gmail.com>
2019-04-21 18:48:26 -0300

    refactor(themes/forms.py): remove formresubmit widget

--- themes/forms.py
+++ themes/forms.py
@@ -13,76 +13,80 @@ Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título
 # coding=utf-8
 from django import forms
 
-from resubmit.widgets import ResubmitFileWidget
-
 from .models import Themes
 
+
 class BasicElemetsForm(forms.ModelForm):
-	MAX_UPLOAD_SIZE = 2*1024*1024
+    MAX_UPLOAD_SIZE = 2 * 1024 * 1024
 
-	def clean_favicon(self):
-		image = self.cleaned_data.get('favicon', False)
+    def clean_favicon(self):
+        image = self.cleaned_data.get('favicon', False)
 
-		if image:
-			if hasattr(image, '_size'):
-				if image._size > self.MAX_UPLOAD_SIZE:
-					self._errors['favicon'] = [_("The image is too large. It should have less than 2MB.")]
+        if image:
+            if hasattr(image, '_size'):
+                if image._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['favicon'] = [
+                        _("The image is too large. It should have less than 2MB.")]
 
-					return ValueError
+                    return ValueError
 
-		return image
+        return image
 
-	def clean_small_logo(self):
-		image = self.cleaned_data.get('small_logo', False)
+    def clean_small_logo(self):
+        image = self.cleaned_data.get('small_logo', False)
 
-		if image:
-			if hasattr(image, '_size'):
-				if image._size > self.MAX_UPLOAD_SIZE:
-					self._errors['small_logo'] = [_("The image is too large. It should have less than 2MB.")]
+        if image:
+            if hasattr(image, '_size'):
+                if image._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['small_logo'] = [
+                        _("The image is too large. It should have less than 2MB.")]
 
-					return ValueError
+                    return ValueError
 
-		return image
+        return image
 
-	def clean_large_logo(self):
-		image = self.cleaned_data.get('large_logo', False)
+    def clean_large_logo(self):
+        image = self.cleaned_data.get('large_logo', False)
 
-		if image:
-			if hasattr(image, '_size'):
-				if image._size > self.MAX_UPLOAD_SIZE:
-					self._errors['large_logo'] = [_("The image is too large. It should have less than 2MB.")]
+        if image:
+            if hasattr(image, '_size'):
+                if image._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['large_logo'] = [
+                        _("The image is too large. It should have less than 2MB.")]
 
-					return ValueError
+                    return ValueError
 
-		return image
+        return image
 
-	def clean_high_contrast_logo(self):
-		image = self.cleaned_data.get('high_contrast_logo', False)
+    def clean_high_contrast_logo(self):
+        image = self.cleaned_data.get('high_contrast_logo', False)
 
-		if image:
-			if hasattr(image, '_size'):
-				if image._size > self.MAX_UPLOAD_SIZE:
-					self._errors['high_contrast_logo'] = [_("The image is too large. It should have less than 2MB.")]
+        if image:
+            if hasattr(image, '_size'):
+                if image._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['high_contrast_logo'] = [
+                        _("The image is too large. It should have less than 2MB.")]
 
-					return ValueError
+                    return ValueError
 
-		return image
-	
-	class Meta:
-		model = Themes
-		fields = ['title', 'favicon', 'small_logo', 'large_logo', 'high_contrast_logo', 'footer_note']
-		widgets = {
-			'favicon': ResubmitFileWidget(attrs={'accept':'image/*'}),
-			'small_logo': ResubmitFileWidget(attrs={'accept':'image/*'}),
-			'large_logo': ResubmitFileWidget(attrs={'accept':'image/*'}),
-			'high_contrast_logo': ResubmitFileWidget(attrs={'accept':'image/*'}),
-		}
+        return image
+
+    class Meta:
+        model = Themes
+        fields = ['title', 'favicon', 'small_logo', 'large_logo', 'high_contrast_logo',
+                  'footer_note']
+        widgets = {
+            'favicon': forms.FileInput(attrs={'accept': 'image/*'}),
+            'small_logo': forms.FileInput(attrs={'accept': 'image/*'}),
+            'large_logo': forms.FileInput(attrs={'accept': 'image/*'}),
+            'high_contrast_logo': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
+
 
 class CSSStyleForm(forms.ModelForm):
-
-	class Meta:
-		model = Themes
-		fields = ['css_style']
-		widgets = {
-			'css_style': forms.RadioSelect
-		}
+    class Meta:
+        model = Themes
+        fields = ['css_style']
+        widgets = {
+            'css_style': forms.RadioSelect
+        }
