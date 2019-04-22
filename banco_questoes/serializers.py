68eefc204f8b747a0c91902b8f2ff217c8a02ea0
fbormann <felipe.bormann@gmail.com>
2019-04-21 23:52:40 -0300

    refactor(banco_questoes/serializers.py): remove unnecessary imports

--- banco_questoes/serializers.py
+++ banco_questoes/serializers.py
@@ -11,23 +11,19 @@ Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título
 """
 
 import os
-import zipfile
 import time
-from django.db.models import Q
-from django.conf import settings
-from django.core.files import File
-from django.shortcuts import get_object_or_404
 
+from django.conf import settings
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
+from subjects.models import Tag
 from subjects.serializers import TagSerializer
-
-from subjects.models import Tag, Subject
-
 from .models import Question, Alternative
 
+
 class AlternativeSerializer(serializers.ModelSerializer):
-    alt_img = serializers.CharField(required = False, allow_blank = True, max_length = 255)
+    alt_img = serializers.CharField(required=False, allow_blank=True, max_length=255)
 
     def validate(self, data):
         files = self.context.get('files', None)
@@ -41,10 +37,15 @@ class AlternativeSerializer(serializers.ModelSerializer):
 
                     path = files.extract(data["alt_img"], dst_path)
 
-                    new_name = os.path.join("questions", os.path.join("alternatives", "alternative_" + str(time.time()) + os.path.splitext(data["question_img"])[1]))
+                    new_name = os.path.join("questions", os.path.join("alternatives",
+                                                                      "alternative_" + str(
+                                                                          time.time()) +
+                                                                      os.path.splitext(
+                                                                          data["question_img"])[1]))
 
-                    os.rename(os.path.join(dst_path, path), os.path.join(settings.MEDIA_ROOT, new_name))
-                    
+                    os.rename(os.path.join(dst_path, path),
+                              os.path.join(settings.MEDIA_ROOT, new_name))
+
                     data["alt_img"] = new_name
                 else:
                     path = files.extract(data["alt_img"], settings.MEDIA_ROOT)
@@ -59,10 +60,11 @@ class AlternativeSerializer(serializers.ModelSerializer):
         model = Alternative
         exclude = ('question',)
 
+
 class QuestionDatabaseSerializer(serializers.ModelSerializer):
-    categories = TagSerializer(many = True)
-    alt_question = AlternativeSerializer('get_files', many = True)
-    question_img = serializers.CharField(required = False, allow_blank = True, max_length = 255)
+    categories = TagSerializer(many=True)
+    alt_question = AlternativeSerializer('get_files', many=True)
+    question_img = serializers.CharField(required=False, allow_blank=True, max_length=255)
 
     def get_subject(self, obj):
         subject = self.context.get("subject", None)
@@ -86,10 +88,12 @@ class QuestionDatabaseSerializer(serializers.ModelSerializer):
 
                     path = files.extract(data["question_img"], dst_path)
 
-                    new_name = os.path.join("questions","question_" + str(time.time()) + os.path.splitext(data["question_img"])[1])
+                    new_name = os.path.join("questions", "question_" + str(time.time()) +
+                                            os.path.splitext(data["question_img"])[1])
 
-                    os.rename(os.path.join(dst_path, path), os.path.join(settings.MEDIA_ROOT, new_name))
-                    
+                    os.rename(os.path.join(dst_path, path),
+                              os.path.join(settings.MEDIA_ROOT, new_name))
+
                     data["question_img"] = new_name
                 else:
                     path = files.extract(data["question_img"], settings.MEDIA_ROOT)
@@ -102,7 +106,7 @@ class QuestionDatabaseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Question
-        exclude = ('subject', )
+        exclude = ('subject',)
 
     def create(self, data):
         question_data = data
@@ -122,13 +126,13 @@ class QuestionDatabaseSerializer(serializers.ModelSerializer):
         for tag in tags:
             if not tag["name"] == "":
                 if tag["id"] == "":
-                    tag = Tag.objects.create(name = tag["name"])
+                    tag = Tag.objects.create(name=tag["name"])
                 else:
-                    tag = get_object_or_404(Tag, id = tag["id"])
+                    tag = get_object_or_404(Tag, id=tag["id"])
 
                 question.categories.add(tag)
 
         for alt in alternatives:
-            Alternative.objects.create(question = question, **alt)
+            Alternative.objects.create(question=question, **alt)
 
         return question
