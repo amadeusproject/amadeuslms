f4882f56eecfa9efc2f35c72c4d17ce3ca7bb6a5
fbormann <felipe.bormann@gmail.com>
2019-04-21 23:50:31 -0300

    refactor(mural/forms.py): remove resubmit widget

--- mural/forms.py
+++ mural/forms.py
@@ -12,135 +12,142 @@ Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título
 
 # coding=utf-8
 from django import forms
-from django.utils.translation import ugettext_lazy as _, pgettext_lazy
-from django.utils.html import strip_tags
 from django.db.models import Q
-
-from resubmit.widgets import ResubmitFileWidget
+from django.utils.html import strip_tags
+from django.utils.translation import ugettext_lazy as _, pgettext_lazy
 
 from topics.models import Resource
-
 from .models import GeneralPost, CategoryPost, SubjectPost, Comment
 
+
 class Validation(forms.ModelForm):
-	MAX_UPLOAD_SIZE = 5*1024*1024
+    MAX_UPLOAD_SIZE = 5 * 1024 * 1024
 
-	def __init__(self, *args, **kwargs):
-		super(Validation, self).__init__(*args, **kwargs)
-		CHOICES = (("comment", pgettext_lazy("form","Comment")), ("help", pgettext_lazy("form","Ask for Help")))
-		self.fields['action'].choices = CHOICES
+    def __init__(self, *args, **kwargs):
+        super(Validation, self).__init__(*args, **kwargs)
+        CHOICES = (("comment", pgettext_lazy("form", "Comment")),
+                   ("help", pgettext_lazy("form", "Ask for Help")))
+        self.fields['action'].choices = CHOICES
 
-	def clean_post(self):
-		post = self.cleaned_data.get('post', '')
-		cleaned_post = strip_tags(post)
+    def clean_post(self):
+        post = self.cleaned_data.get('post', '')
+        cleaned_post = strip_tags(post)
 
-		if cleaned_post == '':
-			self._errors['post'] = [_('This field is required.')]
+        if cleaned_post == '':
+            self._errors['post'] = [_('This field is required.')]
 
-			return ValueError
+            return ValueError
 
-		return post
+        return post
 
-	def clean_image(self):
-		image = self.cleaned_data.get('image', False)
+    def clean_image(self):
+        image = self.cleaned_data.get('image', False)
 
-		if image:
-			if hasattr(image, '_size'):
-				if image._size > self.MAX_UPLOAD_SIZE:
-					self._errors['image'] = [_("The image is too large. It should have less than 5MB.")]
+        if image:
+            if hasattr(image, '_size'):
+                if image._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['image'] = [
+                        _("The image is too large. It should have less than 5MB.")]
 
-					return ValueError
+                    return ValueError
 
-		return image
+        return image
 
 
 class GeneralPostForm(Validation):
+    class Meta:
+        model = GeneralPost
+        fields = ['action', 'post', 'image']
 
-	class Meta:
-		model = GeneralPost
-		fields = ['action', 'post', 'image']
+        widgets = {
+            'action': forms.RadioSelect,
+            'post': forms.Textarea,
+            'image': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
 
-		widgets = {
-			'action': forms.RadioSelect,
-			'post': forms.Textarea,
-			'image': ResubmitFileWidget(attrs={'accept':'image/*'}),
-		}
 
 class CategoryPostForm(Validation):
-	class Meta:
-		model = CategoryPost
-		fields = ['action', 'post', 'image']
-		widgets = {
-			'action': forms.RadioSelect,
-			'post': forms.Textarea,
-			'image': ResubmitFileWidget(attrs={'accept':'image/*'}),
-		}
+    class Meta:
+        model = CategoryPost
+        fields = ['action', 'post', 'image']
+        widgets = {
+            'action': forms.RadioSelect,
+            'post': forms.Textarea,
+            'image': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
+
 
 class SubjectPostForm(Validation):
-	def __init__(self, *args, **kwargs):
-		super(SubjectPostForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(SubjectPostForm, self).__init__(*args, **kwargs)
 
-		user = kwargs['initial'].get('user', None)
-		subject = kwargs['initial'].get('subject', None)
+        user = kwargs['initial'].get('user', None)
+        subject = kwargs['initial'].get('subject', None)
 
-		if not kwargs['instance'] is None:
-			subject = self.instance.space
+        if not kwargs['instance'] is None:
+            subject = self.instance.space
 
-		if user.is_staff:
-			self.fields['resource'].choices = [(r.id, str(r)) for r in Resource.objects.filter(Q(topic__subject = subject))]
-		else:
-			self.fields['resource'].choices = [(r.id, str(r)) for r in Resource.objects.filter(Q(topic__subject = subject) & (Q(all_students = True) | Q(students = user) | Q(groups__participants = user)))]
+        if user.is_staff:
+            self.fields['resource'].choices = [(r.id, str(r)) for r in
+                                               Resource.objects.filter(Q(topic__subject=subject))]
+        else:
+            self.fields['resource'].choices = [(r.id, str(r)) for r in Resource.objects.filter(
+                Q(topic__subject=subject) & (Q(all_students=True) | Q(students=user) | Q(
+                    groups__participants=user)))]
 
-		self.fields['resource'].choices.append(("", _("Choose an especific resource")))
+        self.fields['resource'].choices.append(("", _("Choose an especific resource")))
 
-	class Meta:
-		model = SubjectPost
-		fields = ['action', 'resource', 'post', 'image']
-		widgets = {
-			'action': forms.RadioSelect,
-			'post': forms.Textarea,
-			'image': ResubmitFileWidget(attrs={'accept':'image/*'}),
-		}
+    class Meta:
+        model = SubjectPost
+        fields = ['action', 'resource', 'post', 'image']
+        widgets = {
+            'action': forms.RadioSelect,
+            'post': forms.Textarea,
+            'image': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
+
 
 class ResourcePostForm(Validation):
-	class Meta:
-		model = SubjectPost
-		fields = ['action', 'post', 'image']
-		widgets = {
-			'action': forms.RadioSelect,
-			'post': forms.Textarea,
-			'image': ResubmitFileWidget(attrs={'accept':'image/*'}),
-		}
+    class Meta:
+        model = SubjectPost
+        fields = ['action', 'post', 'image']
+        widgets = {
+            'action': forms.RadioSelect,
+            'post': forms.Textarea,
+            'image': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
+
 
 class CommentForm(forms.ModelForm):
-	MAX_UPLOAD_SIZE = 5*1024*1024
+    MAX_UPLOAD_SIZE = 5 * 1024 * 1024
 
-	def clean_comment(self):
-		comment = self.cleaned_data.get('comment', '')
-		cleaned_comment = strip_tags(comment)
+    def clean_comment(self):
+        comment = self.cleaned_data.get('comment', '')
+        cleaned_comment = strip_tags(comment)
 
-		if cleaned_comment == '':
-			self._errors['comment'] = [_('This field is required.')]
+        if cleaned_comment == '':
+            self._errors['comment'] = [_('This field is required.')]
 
-			return ValueError
+            return ValueError
 
-		return comment
+        return comment
 
-	def clean_image(self):
-		image = self.cleaned_data.get('image', False)
+    def clean_image(self):
+        image = self.cleaned_data.get('image', False)
 
-		if image:
-			if hasattr(image, '_size'):
-				if image._size > self.MAX_UPLOAD_SIZE:
-					self._errors['image'] = [_("The image is too large. It should have less than 5MB.")]
+        if image:
+            if hasattr(image, '_size'):
+                if image._size > self.MAX_UPLOAD_SIZE:
+                    self._errors['image'] = [
+                        _("The image is too large. It should have less than 5MB.")]
 
-					return ValueError
+                    return ValueError
 
-		return image
+        return image
 
-	class Meta:
-		model = Comment
-		fields = ['comment', 'image']
-		widgets = {
-			'image': ResubmitFileWidget(attrs={'accept':'image/*'}),
-		}
+    class Meta:
+        model = Comment
+        fields = ['comment', 'image']
+        widgets = {
+            'image': forms.FileInput(attrs={'accept': 'image/*'}),
+        }
