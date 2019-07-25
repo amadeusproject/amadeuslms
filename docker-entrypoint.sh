9d5495d79f2f19ef3b15c3770df798fd462a7df3
fbormann <felipe.bormann@gmail.com>
2019-07-24 23:58:34 -0300

    update docker requirements

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,4 @@ python3 manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate 
 python3 manage.py compilemessages
-gunicorn --bind :8000 amadeus.wsgi:application
-# python3 manage.py runserver 0.0.0.0:8000
+gunicorn --bind :$PORT amadeus.wsgi:application
