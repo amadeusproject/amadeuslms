232ca1d953307fb63fdbd215c03fe9b1a9addc1e
fbormann <felipe.bormann@gmail.com>
2019-08-12 23:23:52 -0300

    refactor(categories/forms.py): remove unnaccent filter, it does not exist on django 2.x

--- categories/forms.py
+++ categories/forms.py
@@ -15,21 +15,23 @@ from .models import Category
 
 from django.utils.translation import ugettext_lazy as _
 
+
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ( 'name', 'description', 'visible', 'coordinators', )
+        fields = ('name', 'description', 'visible', 'coordinators',)
         widgets = {
             'description': forms.Textarea,
-            'coordinators' : forms.SelectMultiple,
+            'coordinators': forms.SelectMultiple,
         }
-        
+
     def clean_name(self):
         name = self.cleaned_data.get('name')
         if self.instance.id:
-            same_name = Category.objects.filter(name__unaccent__iexact = name).exclude(id = self.instance.id)
+            same_name = Category.objects.filter(name__iexact=name).exclude(
+                id=self.instance.id)
         else:
-            same_name = Category.objects.filter(name__unaccent__iexact = name)
+            same_name = Category.objects.filter(name__iexact=name)
 
         if same_name.count() > 0:
             self._errors['name'] = [_('There is another category with this name, try another one.')]
