2ee256e89d1490e77c2835d9a38259d19f55d806
fbormann <felipe.bormann@gmail.com>
2019-09-15 23:04:58 -0300

    refactor(log/decorators.py): fix pep style of code

--- log/decorators.py
+++ log/decorators.py
@@ -10,7 +10,6 @@ Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA
 Você deve ter recebido uma cópia da Licença Pública Geral GNU, sob o título "LICENSE", junto com este programa, se não, escreva para a Fundação do Software Livre (FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
 """
 
-
 import json
 import time
 from functools import wraps
@@ -18,97 +17,97 @@ from django.shortcuts import get_object_or_404
 
 from .models import Log
 
-def log_decorator(log_component = '', log_action = '', log_resource = ''):
 
-	def _log_decorator(view_function):
+def log_decorator(log_component='', log_action='', log_resource=''):
+    def _log_decorator(view_function):
 
-		def _decorator(request, *args, **kwargs):
-			user = None
+        def _decorator(request, *args, **kwargs):
+            user = None
 
-			#Get user before logout
-			if request.user.is_authenticated:
-				user = request.user				
+            # Get user before logout
+            if request.user.is_authenticated:
+                user = request.user
 
-			response = view_function(request, *args, **kwargs)
+            response = view_function(request, *args, **kwargs)
 
-			#Get user after login
-			if user is None and request.user.is_authenticated:
-				user = request.user
+            # Get user after login
+            if user is None and request.user.is_authenticated:
+                user = request.user
 
-			log_context = {}
+            log_context = {}
 
-			if hasattr(request, 'log_context'):
-				log_context = request.log_context
+            if hasattr(request, 'log_context'):
+                log_context = request.log_context
 
-			if user:				
-				log = Log()
-				log.user = str(user)
-				log.user_id = user.id
-				log.user_email = user.email
-				log.component = log_component
-				log.context = log_context
-				log.action = log_action
-				log.resource = log_resource
+            if user:
+                log = Log()
+                log.user = str(user)
+                log.user_id = user.id
+                log.user_email = user.email
+                log.component = log_component
+                log.context = log_context
+                log.action = log_action
+                log.resource = log_resource
 
-				log.save()
+                log.save()
 
-			return response
+            return response
 
-		return wraps(view_function)(_decorator)
+        return wraps(view_function)(_decorator)
 
-	return _log_decorator
+    return _log_decorator
 
-def log_decorator_ajax(log_component = '', log_action = '', log_resource = ''):
 
-	def _log_decorator_ajax(view_function):
+def log_decorator_ajax(log_component='', log_action='', log_resource=''):
+    def _log_decorator_ajax(view_function):
 
-		def _decorator(request, *args, **kwargs):
-			view_action = request.GET.get("action")
+        def _decorator(request, *args, **kwargs):
+            view_action = request.GET.get("action")
 
-			if view_action == 'open':
-				if request.user.is_authenticated:
-					
-					log = Log()
-					log.user = str(request.user)
-					log.user_id = request.user.id
-					log.user_email = request.user.email
-					log.component = log_component
-					log.context = ""
-					log.action = log_action
-					log.resource = log_resource
+            if view_action == 'open':
+                if request.user.is_authenticated:
 
-					log.save()
+                    log = Log()
+                    log.user = str(request.user)
+                    log.user_id = request.user.id
+                    log.user_email = request.user.email
+                    log.component = log_component
+                    log.context = ""
+                    log.action = log_action
+                    log.resource = log_resource
 
-					response = view_function(request, *args, **kwargs)
-					
-					log_context = {}
+                    log.save()
 
-					if hasattr(request, 'log_context'):
-						log_context = request.log_context
+                    response = view_function(request, *args, **kwargs)
 
-					log = Log.objects.latest('id')
-					log.context = log_context
-					log.save()
-					
-			elif view_action == 'close':
-				if request.user.is_authenticated:
-					log = get_object_or_404(Log, id = request.GET.get('log_id'))
+                    log_context = {}
 
-					if type(log.context) == dict:
-						log_context = log.context
-					else:
-						log_context = json.loads(log.context)
+                    if hasattr(request, 'log_context'):
+                        log_context = request.log_context
 
-					log_context['timestamp_end'] = str(int(time.time()))
+                    log = Log.objects.latest('id')
+                    log.context = log_context
+                    log.save()
 
-					log.context = log_context
+            elif view_action == 'close':
+                if request.user.is_authenticated:
+                    log = get_object_or_404(Log, id=request.GET.get('log_id'))
 
-					log.save()
+                    if type(log.context) == dict:
+                        log_context = log.context
+                    else:
+                        log_context = json.loads(log.context)
 
-					response = view_function(request, *args, **kwargs)
+                    log_context['timestamp_end'] = str(int(time.time()))
 
-			return response
+                    log.context = log_context
 
-		return wraps(view_function)(_decorator)
+                    log.save()
 
-	return _log_decorator_ajax
+                    response = view_function(request, *args, **kwargs)
+
+            return response
+
+        return wraps(view_function)(_decorator)
+
+    return _log_decorator_ajax
