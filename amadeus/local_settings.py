70b55dc16abe33f0672b9b62ff432cf8d2deef45
Zambom <erikzambom@hotmail.com>
2019-10-04 03:36:00 -0300

    Adjusting translations

--- amadeus/local_settings.py
+++ amadeus/local_settings.py
@@ -1,9 +1,19 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DEBUG = True
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amadeus',
+        'USER': 'amadeus_admin',
+        'PASSWORD': 'amadeus',
         'HOST': 'db',
-        'PORT': 5432,
+        'PORT': '5432',
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
