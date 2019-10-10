3e37780b0e3fcc0ee48ad47020aa4ff955a5b2f2
Zambom <erikzambom@hotmail.com>
2019-10-10 02:31:01 -0300

    Removing unnecessary elastic configuration for subject and topics

--- subjects/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from .models import Tag
-
-@receiver(post_save, sender=Tag)
-def index_tag(sender, instance, **kwargs):
-    instance.indexing()
--- topics/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-from .models import Resource
-
-@receiver(post_save, sender=Resource)
-def index_resource(sender, instance, **kwargs):
-    instance.indexing()
