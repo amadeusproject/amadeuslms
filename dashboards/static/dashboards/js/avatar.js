44533cd2a2ae8767d9f7fe9790a673456246d0fd
Zambom <erikzambom@hotmail.com>
2020-03-19 10:21:25 -0300

    Changing constructor to receive a object with default parameters

--- dashboards/static/dashboards/js/avatar.js
+++ dashboards/static/dashboards/js/avatar.js
@@ -1,6 +1,7 @@
 class Avatar {
-  constructor(trigger, introAudios, cloudAudios) {
+  constructor({ trigger = $("#avatar"), cloudTrigger = $("#cloudyInfo"), introAudios = [], cloudAudios = [] } = {}) {
     this.$trigger = trigger;
+    this.$cloudTrigger = cloudTrigger;
 
     this.intro = introAudios;
     this.cloud = cloudAudios;
@@ -8,10 +9,23 @@ class Avatar {
   }
 
   Init() {
-    $('.avatarBox').show();
+    $(".avatarBox").show();
     this.playIntro();
 
     this.$trigger.parent().hide();
+
+    if (this.cloud.length > 0) {
+      this.$cloudTrigger
+        .removeAttr("data-toggle")
+        .removeAttr("data-original-title")
+        .css("cursor", "pointer");
+
+      let avatar = this;
+
+      this.$cloudTrigger.on("click", function() {
+        avatar.playCloud();
+      });
+    }
   }
 
   playIntro() {
@@ -20,17 +34,17 @@ class Avatar {
 
   playCloud() {
     if (!this.playing) {
-      $('#tagCloudy').css('box-shadow', '0 0 0 999px rgba(0, 0, 0, 0.5)');
-      $('.avatarBox').css('z-index', '9');
-      $('#otherIndicators svg').css('filter', 'brightness(0.5)');
-      $('.graph-container svg').css('filter', 'brightness(0.5)');
+      $("#tagCloudy").css("box-shadow", "0 0 0 999px rgba(0, 0, 0, 0.5)");
+      $(".avatarBox").css("z-index", "9");
+      $("#otherIndicators svg").css("filter", "brightness(0.5)");
+      $(".graph-container svg").css("filter", "brightness(0.5)");
 
       let totalTime = this.setAudios(this.cloud);
 
       setTimeout(function() {
-        $('#tagCloudy').css('box-shadow', 'none');
-        $('#otherIndicators svg').css('filter', 'brightness(1)');
-        $('.graph-container svg').css('filter', 'brightness(1)');
+        $("#tagCloudy").css("box-shadow", "none");
+        $("#otherIndicators svg").css("filter", "brightness(1)");
+        $(".graph-container svg").css("filter", "brightness(1)");
       }, totalTime);
     }
   }
@@ -49,20 +63,20 @@ class Avatar {
       }
 
       setTimeout(function() {
-        let $ballon = $('.ballon').find('p');
+        let $ballon = $(".ballon").find("p");
 
         $($ballon).html(file.text);
 
-        $('.ballon').show();
+        $(".ballon").show();
 
         // audio.play();
       }, totalTime);
 
-      totalTime += (file.duration * 1000);
+      totalTime += file.duration * 1000;
     });
 
     setTimeout(function() {
-      $('.ballon').hide();
+      $(".ballon").hide();
       avatar.playing = false;
     }, totalTime);
 
@@ -70,16 +84,15 @@ class Avatar {
   }
 
   showResourcesTable(link, tagName) {
-    d3.select('#modal_cloudy_loading_ball').style('display', 'inherit');
-    d3.select('#modal-table').style('display', 'none');
+    d3.select("#modal_cloudy_loading_ball").style("display", "inherit");
+    d3.select("#modal-table").style("display", "none");
 
-    const modal = document.querySelector('#tagModal');
-    const container = d3.select('#resources-list');
+    const modal = document.querySelector("#tagModal");
+    const container = d3.select("#resources-list");
 
-    modal.querySelector('#modalTittle').innerText =
-        `Tag: ${tagName.toUpperCase()}`;
+    modal.querySelector("#modalTittle").innerText = `Tag: ${tagName.toUpperCase()}`;
 
-    container.selectAll('.resource').remove();
+    container.selectAll(".resource").remove();
 
     $.get(link, dataset => {
       dataset = dataset.sort((d1, d2) => {
@@ -100,21 +113,17 @@ class Avatar {
         }
 
         const p1 = d1.qtd_my_access / d1.qtd_access,
-              p2 = d2.qtd_my_access / d2.qtd_access;
+          p2 = d2.qtd_my_access / d2.qtd_access;
 
-        return p1 > p2 ? 1 :
-                         p1 < p2 ? -1 :
-                                   d1.qtd_access < d2.qtd_access ?
-                                   1 :
-                                   d1.qtd_access > d2.qtd_access ? -1 : 0;
+        return p1 > p2 ? 1 : p1 < p2 ? -1 : d1.qtd_access < d2.qtd_access ? 1 : d1.qtd_access > d2.qtd_access ? -1 : 0;
       });
 
-      makeTable(dataset, '#table-container', '#resources_pag', 10);
+      makeTable(dataset, "#table-container", "#resources_pag", 10);
 
-      d3.select('#modal_cloudy_loading_ball').style('display', 'none');
-      d3.select('#modal-table').style('display', 'inherit');
+      d3.select("#modal_cloudy_loading_ball").style("display", "none");
+      d3.select("#modal-table").style("display", "inherit");
     });
 
-    $('#tagModal').modal('show');
+    $("#tagModal").modal("show");
   }
-};
+}
