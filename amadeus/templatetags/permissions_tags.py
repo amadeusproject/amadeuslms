f2d9cfe21e06cda9eafb64b06fe5c7a16f3be3b7
Zambom <erikzambom@hotmail.com>
2020-08-03 19:59:57 -0300

    Adjusting analytics menu button visibility

--- amadeus/templatetags/permissions_tags.py
+++ amadeus/templatetags/permissions_tags.py
@@ -16,18 +16,28 @@ from amadeus import permissions
 
 register = template.Library()
 
+
+@register.assignment_tag
+def analytics_permissions(user):
+    return permissions.has_analytics_permissions(user)
+
+
 @register.assignment_tag
 def category_permissions(user, category):
-	return permissions.has_category_permissions(user, category)
+    return permissions.has_category_permissions(user, category)
+
 
 @register.assignment_tag
 def subject_permissions(user, subject):
-	return permissions.has_subject_permissions(user, subject)
+    return permissions.has_subject_permissions(user, subject)
+
 
 @register.assignment_tag
 def subject_view_permissions(user, subject):
-	return permissions.has_subject_view_permissions(user, subject)
+    return permissions.has_subject_view_permissions(user, subject)
+
 
 @register.assignment_tag
 def resource_permissions(user, resource):
-	return permissions.has_resource_permissions(user, resource)
+    return permissions.has_resource_permissions(user, resource)
+
