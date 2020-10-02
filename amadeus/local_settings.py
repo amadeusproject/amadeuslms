af7192ffe9d14149df2b12d44cef84306f9c41fa
Erik Zambom <erikzambom@hotmail.com>
2020-10-01 23:59:06 -0300

    Update local_settings.py

--- amadeus/local_settings.py
+++ amadeus/local_settings.py
@@ -5,22 +5,44 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "amadeus",
-        "USER": "amadeus_admin",
-        "PASSWORD": "amadeus",
-        "HOST": "db",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amadeuslms',
+        'USER': 'amadeus',
+        'PASSWORD': 'amadeuslms',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "asgiref.inmemory.ChannelLayer",
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "CONFIG": {"hosts": [("localhost", 6379)], },
         "ROUTING": "amadeus.routing.channel_routing",
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+FILE_UPLOAD_PERMISSIONS = 0o644
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/amadeus/django_debug.log'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True
+        },
     }
 }
