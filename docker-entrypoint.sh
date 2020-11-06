01e94e41e2524e0be0f51f25d560c060e74b0d02
Jailson Dias <jailson.diasc@gmail.com>
2020-11-06 18:00:25 -0300

    fix news migrate pk error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 python3 manage.py makemigrations
-python3 manage.py migrate 
+python3 manage.py migrate users
+python3 manage.py migrate
 python3 manage.py compilemessages
 python3 manage.py collectstatic --no-input
 python3 manage.py runserver 0.0.0.0:8000
