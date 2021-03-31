f7b351748c78e4b36658cab09ab61039abeea2a7
Jailson Dias <jailson.diasc@gmail.com>
2021-03-31 13:39:01 -0300

    update granito host

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(localhost.com www.localhost.com)
+domains=(conectagranito.net.br www.conectagranito.net.br)
 rsa_key_size=4096
 data_path="./certbot"
 email="lms.granito21.pe@gmail.com" # Adding a valid address is strongly recommended
