5cbbc29f6ee3a646b7b85b18ce603b9247b3681e
Jailson Dias <jailson.diasc@gmail.com>
2021-05-21 07:50:19 -0300

    add cron to notifications and goals

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,5 @@ python3 manage.py migrate users
 python3 manage.py migrate
 python3 manage.py compilemessages
 python3 manage.py collectstatic --no-input
+python3 manage.py crontab add
 python3 manage.py runserver 0.0.0.0:8000
